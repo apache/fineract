b016030267a36ac25bb8ef0311a90819287cc48a
John Woodlock <john.woodlock@gmail.com>
2012-05-10 13:08:30 +1000

    WIP restructure of extradata api

---
+++ mifosng-data/src/main/java/org/mifosng/data/ExtraDatasetRow.java
@@ -0,0 +1,48 @@
+package org.mifosng.data;
+
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class ExtraDatasetRow {
+
+	private Integer id;
+	private String name;
+	private String type;
+	protected ExtraDatasetRow() {
+		//
+	}
+
+	public ExtraDatasetRow(final Integer id, final String name, final String type) {
+		this.id = id;
+		this.name = name;
+		this.type = type;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+
+}
