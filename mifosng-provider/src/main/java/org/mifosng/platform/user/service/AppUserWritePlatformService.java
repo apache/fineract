0fb05315ea09ae3f4f745cb35a63934999e973e5
Keith Woodlock <keithwoodlock@gmail.com>
2012-05-28 22:15:38 +0100

    add authorization to role and user services.

--- mifosng-provider/src/main/java/org/mifosng/platform/user/service/AppUserWritePlatformService.java
+++ mifosng-provider/src/main/java/org/mifosng/platform/user/service/AppUserWritePlatformService.java
@@ -1,12 +1,16 @@
 package org.mifosng.platform.user.service;
 
 import org.mifosng.data.command.UserCommand;
+import org.springframework.security.access.prepost.PreAuthorize;
 
 public interface AppUserWritePlatformService {
 
+	@PreAuthorize(value = "hasRole('USER_ADMINISTRATION_SUPER_USER_ROLE')")
 	Long createUser(final UserCommand command);
 	
+	@PreAuthorize(value = "hasRole('USER_ADMINISTRATION_SUPER_USER_ROLE')")
 	Long updateUser(final UserCommand command);
 	
+	@PreAuthorize(value = "hasRole('USER_ADMINISTRATION_SUPER_USER_ROLE')")
 	void deleteUser(final Long userId);
 }
--- mifosng-provider/src/main/java/org/mifosng/platform/user/service/RoleWritePlatformService.java
+++ mifosng-provider/src/main/java/org/mifosng/platform/user/service/RoleWritePlatformService.java
@@ -1,10 +1,13 @@
 package org.mifosng.platform.user.service;
 
 import org.mifosng.data.command.RoleCommand;
+import org.springframework.security.access.prepost.PreAuthorize;
 
 public interface RoleWritePlatformService {
 
+	@PreAuthorize(value = "hasRole('USER_ADMINISTRATION_SUPER_USER_ROLE')")
 	Long createRole(RoleCommand command);
 	
+	@PreAuthorize(value = "hasRole('USER_ADMINISTRATION_SUPER_USER_ROLE')")
 	Long updateRole(RoleCommand command);
 }
