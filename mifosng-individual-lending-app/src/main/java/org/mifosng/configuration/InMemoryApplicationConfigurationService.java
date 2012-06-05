878c07f441c586232bc52fd98ec3fc3849532fe2
Keith Woodlock <keithwoodlock@gmail.com>
2012-06-05 17:42:09 +0100

    update url to demo url

--- mifosng-individual-lending-app/src/main/java/org/mifosng/configuration/InMemoryApplicationConfigurationService.java
+++ mifosng-individual-lending-app/src/main/java/org/mifosng/configuration/InMemoryApplicationConfigurationService.java
@@ -8,7 +8,10 @@ import org.springframework.stereotype.Service;
 @Service
 public class InMemoryApplicationConfigurationService implements ApplicationConfigurationService {
 
-	private String platformApiUrl = "https://localhost:8443/mifosng-provider/api/v1/";
+	// demo url
+	private String platformApiUrl = "https://ec2-46-137-62-163.eu-west-1.compute.amazonaws.com:8443/mifosng-provider/api/v1/";
+	// dev url
+//	private String platformApiUrl = "https://localhost:8443/mifosng-provider/api/v1/";
 			
 	public InMemoryApplicationConfigurationService() {
 		//
