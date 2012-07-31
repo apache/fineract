78aa9eeadde8b39f8639ae8b21fc56ffe3345c11
John Woodlock <john.woodlock@gmail.com>
2012-08-01 01:12:00 +1000

    ddl for portfolio_group table

--- mifosng-db/0001a-mifos-platform-base-ddl-schema.sql
+++ mifosng-db/0001a-mifos-platform-base-ddl-schema.sql
@@ -14,6 +14,7 @@ DROP TABLE IF EXISTS `portfolio_loan_transaction`;
 DROP TABLE IF EXISTS `portfolio_loan_repayment_schedule`;
 DROP TABLE IF EXISTS `portfolio_loan`;
 DROP TABLE IF EXISTS `portfolio_client`;
+DROP TABLE IF EXISTS `portfolio_group`;
 DROP TABLE IF EXISTS `portfolio_product_loan`;
 DROP TABLE IF EXISTS `portfolio_product_savings`;
 -- organisation related tables
@@ -207,6 +208,21 @@ CREATE TABLE `admin_appuser_role` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- DDL for portfolio tables
+CREATE TABLE `portfolio_group` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) DEFAULT NULL,
+  `external_id` varchar(100) DEFAULT NULL,
+  `createdby_id` bigint(20) DEFAULT NULL,
+  `created_date` datetime DEFAULT NULL,
+  `lastmodified_date` datetime DEFAULT NULL,
+  `lastmodifiedby_id` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FKJPWG000000000003` (`createdby_id`),
+  KEY `FKJPWG000000000004` (`lastmodifiedby_id`),
+  CONSTRAINT `FKJPWG000000000003` FOREIGN KEY (`createdby_id`) REFERENCES `admin_appuser` (`id`),
+  CONSTRAINT `FKJPWG000000000004` FOREIGN KEY (`lastmodifiedby_id`) REFERENCES `admin_appuser` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 CREATE TABLE `portfolio_client` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `is_deleted` tinyint(1) NOT NULL DEFAULT 0,
