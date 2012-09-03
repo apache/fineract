a337b6574cd59b3537c8402126136d2cb2fe02ea
John Woodlock <john.woodlock@gmail.com>
2012-09-03 14:27:50 +1000

    update install ddl with changes

---
+++ mifosng-db/patches/patch-0001-insert-special-permissions.sql
@@ -0,0 +1,7 @@
+
+insert into m_permission (group_enum, code, default_description, default_name)
+values (0, "ALL_FUNCTIONS", "An application user will have permission to execute all tasks.", "ALL");
+
+
+insert into m_permission (group_enum, code, default_description, default_name)
+values (0, "ALL_FUNCTIONS_READ", "An application user will have permission to execute all read tasks.", "ALL READ");
