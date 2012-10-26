82a23d7413e0d62522316af55178d75f5177fcbc
Vishwas Babu AJ <vishwas@confluxtechnologies.com>
2012-10-26 17:53:07 +0530

    Add new entities for document management validation

--- mifosng-provider/src/main/java/org/mifosng/platform/common/ApplicationConstants.java
+++ mifosng-provider/src/main/java/org/mifosng/platform/common/ApplicationConstants.java
@@ -4,12 +4,12 @@ public class ApplicationConstants {
 
 	public static final Long CLIENT_IDENTITY_CODE_ID = 9L;
 
-	// TODO:Vishwas Need to move these settings to a database flag
+	// TODO:Vishwas Need to move these settings to the Database
 	public static final Integer MAX_FILE_UPLOAD_SIZE_IN_MB = 5;
 
 	/*** Entities for document Management **/
 	public static enum DOCUMENT_MANAGEMENT_ENTITIES {
-		CLIENTs, STAFF, LOANS;
+		CLIENTS, CLIENT_IDENTIFIERS, STAFF, LOANS, SAVINGS;
 		@Override
 		public String toString() {
 			return name().toString().toLowerCase();
