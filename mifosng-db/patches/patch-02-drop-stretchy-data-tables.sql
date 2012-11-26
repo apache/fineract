8f7487340842fc8f1aa103903e61a418691976ce
Keith Woodlock <keithwoodlock@gmail.com>
2012-11-26 11:42:09 +0000

    update backup of demo database after server upgrade.

---
+++ mifosng-db/patches/patch-02-drop-stretchy-data-tables.sql
@@ -0,0 +1,9 @@
+SET foreign_key_checks = 0;
+drop table IF EXISTS `stretchydata_dataset_fields`;
+drop table IF EXISTS `stretchydata_datasettype`;
+drop table IF EXISTS `stretchydata_dataset`;
+
+drop table IF EXISTS `m_client_xadditional information`;
+drop table IF EXISTS `m_client_xhighly improbable info`;
+drop table IF EXISTS `m_loan_xadditional information`;
+SET foreign_key_checks = 1;
