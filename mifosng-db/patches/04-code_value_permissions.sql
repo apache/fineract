0ef0478d220ee9262e8e0c7e8488bcca5e7acd53
ashok-conflux <ashok@confluxtechnologies.com>
2013-02-07 17:19:28 +0530

    WIP Code Value MIFOSX-36 (database changes)

---
+++ mifosng-db/patches/04-code_value_permissions.sql
@@ -0,0 +1,8 @@
+INSERT INTO `m_permission` (`grouping`, `code`, `entity_name`, `action_name`, `can_maker_checker`) 
+VALUES('configuration', 'CREATE_CODEVALUE', 'CODEVALUE', 'CREATE', '0'),
+('configuration', 'READ_CODEVALUE', 'CODEVALUE', 'READ', '0'),
+('configuration', 'UPDATE_CODEVALUE', 'CODEVALUE', 'UPDATE', '0'),
+('configuration', 'DELETE_CODEVALUE', 'CODEVALUE', 'DELETE', '0'),
+('configuration', 'CREATE_CODEVALUE_CHECKER', 'CODEVALUE', 'CREATE', '0'),
+('configuration', 'UPDATE_CODEVALUE_CHECKER', 'CODEVALUE', 'UPDATE', '0'),
+('configuration', 'DELETE_CODEVALUE_CHECKER', 'CODEVALUE', 'DELETE', '0');
