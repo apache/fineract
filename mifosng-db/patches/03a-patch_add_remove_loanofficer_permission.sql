9cb3ce56f9d941166345d81704ac7005a9ecd61c
Keith Woodlock <keithwoodlock@gmail.com>
2013-02-07 13:07:27 +0000

    update base reference data with update and remove loan officer permissions and also add in _CHECKER permission

---
+++ mifosng-db/patches/03a-patch_add_remove_loanofficer_permission.sql
@@ -0,0 +1,2 @@
+INSERT INTO `m_permission` (`grouping`, `code`, `entity_name`, `action_name`, `can_maker_checker`) 
+VALUES ('transaction_loan', 'REMOVELOANOFFICER_LOAN', 'LOAN', 'REMOVELOANOFFICER', 1);
