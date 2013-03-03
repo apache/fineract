ecd913596554adcd30d4480cc549a69b531ead70
Keith Woodlock <keithwoodlock@gmail.com>
2013-03-03 02:27:25 +0000

    update DDL and base reference data with patches 16 and 17.

--- mifosng-db/0001a-mifosplatform-core-ddl-latest.sql
+++ mifosng-db/0001a-mifosplatform-core-ddl-latest.sql
@@ -31,6 +31,7 @@ DROP TABLE IF EXISTS `m_loan_collateral`;
 DROP TABLE IF EXISTS `m_loan_officer_assignment_history`;
 DROP TABLE IF EXISTS `m_loan_repayment_schedule`;
 DROP TABLE IF EXISTS `m_loan_transaction`;
+DROP TABLE IF EXISTS `m_calendar`;
 DROP TABLE IF EXISTS `m_note`;
 DROP TABLE IF EXISTS `m_office`;
 DROP TABLE IF EXISTS `m_office_transaction`;
@@ -148,6 +149,26 @@ CREATE TABLE `x_registered_table` (
   `registered_table_name` varchar(50) NOT NULL,
   `application_table_name` varchar(50) NOT NULL,
   PRIMARY KEY (`registered_table_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `m_calendar` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `entity_id` bigint(20) NOT NULL DEFAULT '0',
+  `entity_type_enum` smallint(5) NOT NULL DEFAULT '0',
+  `title` varchar(50) NOT NULL,
+  `description` varchar(100) DEFAULT NULL,
+  `location` varchar(50) DEFAULT NULL,
+  `start_date` date NOT NULL,
+  `end_date` date DEFAULT NULL,
+  `created_date` datetime DEFAULT NULL,
+  `duration` smallint(6) DEFAULT NULL,
+  `calendar_type_enum` smallint(5) NOT NULL,
+  `repeating` tinyint(1) NOT NULL DEFAULT '0',
+  `recurrence` varchar(100) NOT NULL,
+  `remind_by_enum` smallint(5) DEFAULT NULL,
+  `first_reminder` smallint(11) NOT NULL,
+  `second_reminder` smallint(11) NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ================= end of reference/lookup tables =============
