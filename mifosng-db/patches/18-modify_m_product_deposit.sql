0959a22ea8ba86264142db4686f8ebbc0195984d
Keith Woodlock <keithwoodlock@gmail.com>
2013-03-04 14:41:49 +0000

    update patch.

--- mifosng-db/patches/18-modify_m_product_deposit.sql
+++ mifosng-db/patches/18-modify_m_product_deposit.sql
@@ -1,9 +1,7 @@
 ALTER TABLE m_product_deposit
-  CHANGE `minimum_balance` `min_balance` decimal(19,6) NOT NULL,
-  CHANGE `maximum_balance` `max_balance` decimal(19,6) NOT NULL,
   CHANGE `maturity_default_interest_rate` `default_interest_rate` decimal(19,6) NOT NULL,
   CHANGE `maturity_min_interest_rate` `min_interest_rate` decimal(19,6) NOT NULL,
   CHANGE `maturity_max_interest_rate` `max_interest_rate` decimal(19,6) NOT NULL,
-  CHANGE `min_balance` `min_deposit` decimal(19,6) NOT NULL,
-  CHANGE `max_balance` `max_deposit` decimal(19,6) NOT NULL,
+  CHANGE `minimum_balance` `min_deposit` decimal(19,6) NOT NULL,
+  CHANGE `maximum_balance` `max_deposit` decimal(19,6) NOT NULL,
   ADD COLUMN `default_deposit` decimal(19,6) NOT NULL AFTER `min_deposit`;
