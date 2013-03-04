0bca626252b2cb59f9136a6ec4eee160a230b410
Keith Woodlock <keithwoodlock@gmail.com>
2013-03-04 14:44:28 +0000

    update patch to change order of min, max, deafult fields on schema.

--- mifosng-db/patches/18-modify_m_product_deposit.sql
+++ mifosng-db/patches/18-modify_m_product_deposit.sql
@@ -5,3 +5,7 @@ ALTER TABLE m_product_deposit
   CHANGE `minimum_balance` `min_deposit` decimal(19,6) NOT NULL,
   CHANGE `maximum_balance` `max_deposit` decimal(19,6) NOT NULL,
   ADD COLUMN `default_deposit` decimal(19,6) NOT NULL AFTER `min_deposit`;
+  
+ALTER TABLE `mifostenant-creocore`.`m_product_deposit` 
+CHANGE COLUMN `max_deposit` `max_deposit` DECIMAL(19,6) NOT NULL  AFTER `min_deposit` , 
+CHANGE COLUMN `default_interest_rate` `default_interest_rate` DECIMAL(19,6) NOT NULL  AFTER `max_interest_rate`;
