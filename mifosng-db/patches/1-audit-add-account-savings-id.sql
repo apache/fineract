f579054c3334b10f336f56ceefbbc781475f2927
John Woodlock <john.woodlock@gmail.com>
2013-03-26 00:38:18 +0000

    rename audit savings field to savings_account_id

--- mifosng-db/patches/1-audit-add-account-savings-id.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE `m_portfolio_command_source` 
- ADD COLUMN `savings_account_id` BIGINT(20) NULL DEFAULT NULL  AFTER `loan_id` ;
---
+++ mifosng-db/patches/2-audit-add-account-savings-id.sql
@@ -0,0 +1,4 @@
+
+ALTER TABLE m_portfolio_command_source
+ CHANGE COLUMN `savings_id` `savings_account_id` BIGINT(20) NULL DEFAULT NULL  ; 
+
