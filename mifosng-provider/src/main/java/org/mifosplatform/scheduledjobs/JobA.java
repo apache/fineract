4826c181a7251a202f08e404f77d05c226eee30d
Arjun K <arjunk@thoughtworks.com>
2013-05-02 10:10:30 +0100

    MIFOSX-158: squash arjunk/kaizer commits into one on scheduling spike.

---
+++ mifosng-provider/src/main/java/org/mifosplatform/scheduledjobs/JobA.java
@@ -0,0 +1,12 @@
+package org.mifosplatform.scheduledjobs;
+
+import java.util.Date;
+
+public class JobA {
+
+    public void execute(){
+        System.out.println("Running Job A "+new Date());
+    }
+
+
+}
---
+++ mifosng-provider/src/main/java/org/mifosplatform/scheduledjobs/JobB.java
@@ -0,0 +1,12 @@
+package org.mifosplatform.scheduledjobs;
+
+
+import java.util.Date;
+
+public class JobB {
+
+    public void execute(){
+        System.out.println("Running Job B "+new Date());
+    }
+
+}
