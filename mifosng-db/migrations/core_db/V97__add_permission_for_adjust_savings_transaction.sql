5df9b37c69f490b8f23a94679319aab5ad50bed7
Vishwas Babu A J <vishwas@confluxtechnologies.com>
2013-08-12 22:26:04 +0530

    cleaning up sql file numbers

---
+++ mifosng-db/migrations/core_db/V97__add_permission_for_adjust_savings_transaction.sql
@@ -0,0 +1,1 @@
+INSERT INTO `m_permission` (`grouping`, `code`, `entity_name`, `action_name`, `can_maker_checker`) VALUES ('transaction_savings', 'ADJUSTTRANSACTION_SAVINGSACCOUNT', 'SAVINGSACCOUNT', 'ADJUSTTRANSACTION', 0);
