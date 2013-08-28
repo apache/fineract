e15fa7967fd4a3b6a343a59d34aec332419bdb67
mifoscontributer <mifos.contributer@gmail.com>
2013-08-28 10:58:21 +0530

    Changing predefined posting json for consistenxy

--- mifosng-provider/src/main/java/org/mifosplatform/accounting/journalentry/exception/JournalEntryInvalidException.java
+++ mifosng-provider/src/main/java/org/mifosplatform/accounting/journalentry/exception/JournalEntryInvalidException.java
@@ -16,7 +16,8 @@ public class JournalEntryInvalidException extends AbstractPlatformDomainRuleExce
 
     /*** enum of reasons for invalid Journal Entry **/
     public static enum GL_JOURNAL_ENTRY_INVALID_REASON {
-        FUTURE_DATE, ACCOUNTING_CLOSED, NO_DEBITS_OR_CREDITS, DEBIT_CREDIT_SUM_MISMATCH_WITH_AMOUNT, DEBIT_CREDIT_SUM_MISMATCH, DEBIT_CREDIT_ACCOUNT_OR_AMOUNT_EMPTY, GL_ACCOUNT_DISABLED, GL_ACCOUNT_MANUAL_ENTRIES_NOT_PERMITTED;
+        FUTURE_DATE, ACCOUNTING_CLOSED, NO_DEBITS_OR_CREDITS, DEBIT_CREDIT_SUM_MISMATCH_WITH_AMOUNT, DEBIT_CREDIT_SUM_MISMATCH, DEBIT_CREDIT_ACCOUNT_OR_AMOUNT_EMPTY, GL_ACCOUNT_DISABLED, GL_ACCOUNT_MANUAL_ENTRIES_NOT_PERMITTED,
+        INVALID_DEBIT_OR_CREDIT_ACCOUNTS;
 
         public String errorMessage() {
             if (name().toString().equalsIgnoreCase("FUTURE_DATE")) {
@@ -33,6 +34,8 @@ public class JournalEntryInvalidException extends AbstractPlatformDomainRuleExce
                 return "Both account and amount must be specified for all Debits and Credits";
             } else if (name().toString().equalsIgnoreCase("GL_ACCOUNT_DISABLED")) {
                 return "Target account has been disabled";
+            } else if (name().toString().equalsIgnoreCase("INVALID_DEBIT_OR_CREDIT_ACCOUNTS")) {
+                return "Invalid debit or credit accounts are passed";
             } else if (name().toString().equalsIgnoreCase("GL_ACCOUNT_MANUAL_ENTRIES_NOT_PERMITTED")) { return "Target account does not allow maual adjustments"; }
             return name().toString();
         }
@@ -50,6 +53,8 @@ public class JournalEntryInvalidException extends AbstractPlatformDomainRuleExce
                 return "error.msg.glJournalEntry.invalid.empty.account.or.amount";
             } else if (name().toString().equalsIgnoreCase("GL_ACCOUNT_DISABLED")) {
                 return "error.msg.glJournalEntry.invalid.account.disabled";
+            } else if (name().toString().equalsIgnoreCase("INVALID_DEBIT_OR_CREDIT_ACCOUNTS")) {
+                return "error.msg.glJournalEntry.invalid.debit.or.credit.accounts";
             } else if (name().toString().equalsIgnoreCase("GL_ACCOUNT_MANUAL_ENTRIES_NOT_PERMITTED")) { return "error.msg.glJournalEntry.invalid.account.manual.adjustments.not.permitted"; }
             return name().toString();
         }
