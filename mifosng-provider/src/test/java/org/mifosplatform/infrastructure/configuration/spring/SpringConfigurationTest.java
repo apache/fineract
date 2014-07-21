c30398d84ab5378fcc65699c6b8ee4ca04b83c8c
Michael Vorburger <mike@vorburger.ch>
2014-07-21 22:39:05 +0200

    New SpringConfigurationTest; the first @RunWith(SpringJUnit4ClassRunner) @ContextConfiguration in Mifos X. More of it to come - stay tuned.
    Signed-off-by: Michael Vorburger <mike@vorburger.ch>

---
+++ mifosng-provider/src/test/java/org/mifosplatform/infrastructure/configuration/spring/SpringConfigurationTest.java
@@ -0,0 +1,28 @@
+package org.mifosplatform.infrastructure.configuration.spring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * This integration test ensure that the "production" Spring XML configuration
+ * files (appContext.xml & Co.) are valid. It does not need any database for
+ * that.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = TestsWithoutDatabaseAndNoJobsConfiguration.class)
+public class SpringConfigurationTest {
+
+    /**
+     * This tests if "Aal izz well" (i.e. if we can start-up with the [almost]
+     * "production" Spring XML configuration) by doing nothing - it doesn't have
+     * to, as the SpringJUnit4ClassRunner with this @ContextConfiguration will
+     * automatically fail if e.g. there is any invalid Spring XML, an invalid
+     * bean definition somewhere or anything like that.
+     * 
+     * @see https://www.google.ch/search?q=Aal+izz+well
+     */
+    @Test
+    public void testSpringXMLConfiguration() {}
+}
