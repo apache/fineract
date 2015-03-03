ab4e97821c6f0a5aa033a4442d518534284925df
Vishwas Babu A J <vishwas@confluxtechnologies.com>
2015-03-03 19:14:09 +0530

    fixing issue with patch number 244

--- mifosng-provider/src/main/resources/sql/migrations/core_db/V244__staff_assignment_history_table.sql
+++ mifosng-provider/src/main/resources/sql/migrations/core_db/V244__staff_assignment_history_table.sql
@@ -13,7 +13,7 @@ KEY `FK_m_staff_assignment_history_centre_id_m_group` (`centre_id`),
 KEY `FK_m_staff_assignment_history_m_staff` (`staff_id`),
 CONSTRAINT `FK_m_staff_assignment_history_centre_id_m_group` FOREIGN KEY (`centre_id`) REFERENCES `m_group` (`id`),
 CONSTRAINT `FK_m_staff_assignment_history_m_staff` FOREIGN KEY (`staff_id`) REFERENCES `m_staff` (`id`)
-)
+);
 INSERT INTO stretchy_parameter ( parameter_name, parameter_variable, parameter_label, parameter_displayType, parameter_FormatType, parameter_default, special, selectOne, selectAll, parameter_sql, parent_id) VALUES ('selectCenterId', 'centerId', 'Enter Center Id', 'text', 'string', 'n/a', NULL, NULL, NULL, NULL, NULL);
 INSERT INTO stretchy_report ( report_name, report_type, report_subtype, report_category, report_sql, description, core_report, use_report) VALUES ( 'Staff Assignment History', 'Pentaho', NULL, NULL, NULL, NULL, 0, 1);
 INSERT INTO stretchy_report_parameter ( report_id, parameter_id, report_parameter_name)
