29b663b00b9de1fdb27d5b988e87e5c076b3e954
Emmanuel Nnaa <emmanuelnnaa@musoni.eu>
2016-07-18 11:29:12 +0200

    commit for FINERACT-50 (Add 'mandatory' property to code value entity)

--- fineract-provider/src/main/java/org/apache/fineract/infrastructure/codes/data/CodeValueData.java
+++ fineract-provider/src/main/java/org/apache/fineract/infrastructure/codes/data/CodeValueData.java
@@ -34,35 +34,51 @@ public class CodeValueData implements Serializable {
 
     @SuppressWarnings("unused")
     private final String description;
-    private final boolean isActive;
+    private final boolean active;
+    private final boolean mandatory;
 
-    public static CodeValueData instance(final Long id, final String name, final Integer position, final boolean isActive) {
+    public static CodeValueData instance(final Long id, final String name, final Integer position, 
+            final boolean isActive, final boolean mandatory) {
         String description = null;
-        return new CodeValueData(id, name, position, description,isActive);
+        return new CodeValueData(id, name, position, description, isActive, mandatory);
     }
 
-    public static CodeValueData instance(final Long id, final String name, final String description, final boolean isActive) {
+    public static CodeValueData instance(final Long id, final String name, final String description, 
+            final boolean isActive, final boolean mandatory) {
         Integer position = null;
-        return new CodeValueData(id, name, position, description,isActive);
+        return new CodeValueData(id, name, position, description, isActive, mandatory);
+    }
+    
+    public static CodeValueData instance(final Long id, final String name, final String description, 
+            final boolean isActive) {
+        Integer position = null;
+        boolean mandatory = false;
+        
+        return new CodeValueData(id, name, position, description, isActive, mandatory);
     }
 
     public static CodeValueData instance(final Long id, final String name) {
         String description = null;
         Integer position = null;
         boolean isActive = false;
-        return new CodeValueData(id, name, position, description, isActive);
+        boolean mandatory = false;
+        
+        return new CodeValueData(id, name, position, description, isActive, mandatory);
     }
 
-    public static CodeValueData instance(final Long id, final String name, final Integer position, final String description, final boolean isActive) {
-        return new CodeValueData(id, name, position, description,isActive);
+    public static CodeValueData instance(final Long id, final String name, final Integer position, 
+            final String description, final boolean isActive, final boolean mandatory) {
+        return new CodeValueData(id, name, position, description, isActive, mandatory);
     }
 
-    private CodeValueData(final Long id, final String name, final Integer position, final String description, final boolean isActive) {
+    private CodeValueData(final Long id, final String name, final Integer position, final String description, 
+            final boolean active, final boolean mandatory) {
         this.id = id;
         this.name = name;
         this.position = position;
         this.description = description;
-        this.isActive = isActive;
+        this.active = active;
+        this.mandatory = mandatory;
     }
 
     public Long getId() {
@@ -71,5 +87,19 @@ public class CodeValueData implements Serializable {
 
     public String getName() {
         return this.name;
+    }
+    
+    /**
+     * @return the mandatory
+     */
+    public boolean isMandatory() {
+        return mandatory;
+    }
+
+    /**
+     * @return the active
+     */
+    public boolean isActive() {
+        return active;
     }
 }
