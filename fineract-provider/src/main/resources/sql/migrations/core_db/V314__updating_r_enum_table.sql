8dfcd13641baf0be8fb2c61fe9436d09874b2342
Nazeer Hussain Shaik <nazeer.shaik@confluxtechnologies.com>
2016-08-02 10:32:00 +0530

    Updating version from V306 to V314 and adding Apache License to V314 sql script

--- fineract-provider/src/main/resources/sql/migrations/core_db/V314__updating_r_enum_table.sql
+++ fineract-provider/src/main/resources/sql/migrations/core_db/V314__updating_r_enum_table.sql
@@ -1,3 +1,22 @@
+--
+-- Licensed to the Apache Software Foundation (ASF) under one
+-- or more contributor license agreements. See the NOTICE file
+-- distributed with this work for additional information
+-- regarding copyright ownership. The ASF licenses this file
+-- to you under the Apache License, Version 2.0 (the
+-- "License"); you may not use this file except in compliance
+-- with the License. You may obtain a copy of the License at
+--
+-- http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing,
+-- software distributed under the License is distributed on an
+-- "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+-- KIND, either express or implied. See the License for the
+-- specific language governing permissions and limitations
+-- under the License.
+--
+
 INSERT INTO `r_enum_value` (`enum_name`, `enum_id`, `enum_message_property`, `enum_value`, `enum_type`) VALUES ('loan_transaction_type_enum', 0, 'INVALID', 'INVALID', 0);
 INSERT INTO `r_enum_value` (`enum_name`, `enum_id`, `enum_message_property`, `enum_value`, `enum_type`) VALUES ('loan_transaction_type_enum', 1, 'DISBURSEMENT', 'DISBURSEMENT', 0);
 INSERT INTO `r_enum_value` (`enum_name`, `enum_id`, `enum_message_property`, `enum_value`, `enum_type`) VALUES ('loan_transaction_type_enum', 2, 'REPAYMENT', 'REPAYMENT', 0);
