himadrimitra/caritasmifosx

168 : interest posting on specified date

fineract-provider/src/main/java/org/apache/fineract/portfolio/savings/exception/PostInterestClosingDateException.java
@@ -1,0 +1,10 @@
+package org.apache.fineract.portfolio.savings.exception;
+
+import org.apache.fineract.infrastructure.core.exception.AbstractPlatformResourceNotFoundException;
+
+public class PostInterestClosingDateException extends AbstractPlatformResourceNotFoundException{
+
+    public PostInterestClosingDateException() {
+        super("error.msg.postInterest.notDone", "Please do a post interest on the closing date");
+    }
+}

apache/fineract

fineract-provider/src/main/java/org/apache/fineract/portfolio/savings/exception/PostInterestClosingDateException.java
@@ -1,0 +1,10 @@
+package org.apache.fineract.portfolio.savings.exception;
+
+import org.apache.fineract.infrastructure.core.exception.AbstractPlatformDomainRuleException;
+
+public class PostInterestClosingDateException extends AbstractPlatformDomainRuleException{
+
+    public PostInterestClosingDateException() {
+        super("error.msg.postInterest.notDone", "Please do a post interest on the closing date");
+    }
+}