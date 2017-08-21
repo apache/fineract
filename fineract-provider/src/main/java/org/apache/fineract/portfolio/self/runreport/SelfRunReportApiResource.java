df16dc3cadb35f0552395d2741b63382701b6f3f
Nazeer Hussain Shaik <nazeer.shaik@confluxtechnologies.com>
2017-08-21 17:42:19 +0530

    Self user role and report access

---
+++ fineract-provider/src/main/java/org/apache/fineract/portfolio/self/runreport/SelfRunReportApiResource.java
@@ -0,0 +1,60 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.fineract.portfolio.self.runreport;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+
+import org.apache.fineract.infrastructure.dataqueries.api.RunreportsApiResource;
+import org.apache.fineract.infrastructure.security.service.PlatformSecurityContext;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Path("/self/runreports")
+@Component
+@Scope("singleton")
+public class SelfRunReportApiResource {
+
+    private final PlatformSecurityContext context;
+    private final RunreportsApiResource runreportsApiResource;
+    
+    @Autowired
+    public SelfRunReportApiResource(final PlatformSecurityContext context, final RunreportsApiResource runreportsApiResource) {
+        this.context = context;
+        this.runreportsApiResource = runreportsApiResource;
+    }
+
+    @GET
+    @Path("{reportName}")
+    @Consumes({ MediaType.APPLICATION_JSON })
+    @Produces({ MediaType.APPLICATION_JSON, "text/csv", "application/vnd.ms-excel", "application/pdf", "text/html" })
+    public Response runReport(@PathParam("reportName") final String reportName, @Context final UriInfo uriInfo) {
+        this.context.authenticatedUser();
+        return this.runreportsApiResource.runReport(reportName, uriInfo);
+    }
+
+}
