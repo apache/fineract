b756fb70b1ebf42c464fb4860544161b3d745147
Nazeer Hussain Shaik <nazeer.shaik@confluxtechnologies.com>
2017-12-05 11:36:07 +0530

    License Header is added in V322_1 and V322_2 migration scripts

--- fineract-provider/src/main/resources/sql/migrations/core_db/V322_1__scheduled_email_campaign.sql
+++ fineract-provider/src/main/resources/sql/migrations/core_db/V322_1__scheduled_email_campaign.sql
@@ -16,36 +16,6 @@
 -- specific language governing permissions and limitations
 -- under the License.
 --
-create table if not exists scheduled_email_campaign (
-id bigint(20) NOT NULL AUTO_INCREMENT,
-campaign_name varchar(100) NOT NULL,
-campaign_type int NOT NULL,
-businessRule_id int NOT NULL,
-param_value text,
-status_enum int NOT NULL,
-closedon_date date,
-closedon_userid bigint(20),
-submittedon_date date,
-submittedon_userid bigint(20),
-approvedon_date date,
-approvedon_userid bigint(20),
-recurrence varchar(100),
-next_trigger_date datetime,
-last_trigger_date datetime,
-recurrence_start_date datetime,
-email_subject varchar(100) not null,
-email_message text not null,
-email_attachment_file_format varchar(10) not null,
-stretchy_report_id int not null,
-stretchy_report_param_map text null,
-previous_run_status varchar(10) null,
-previous_run_error_log text null,
-previous_run_error_message text null,
-is_visible tinyint(1) null,
-foreign key (submittedon_userid) references m_appuser(id),
-foreign key (stretchy_report_id) references stretchy_report(id),
-  PRIMARY KEY (id)
-)ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS scheduled_email_messages_outbound (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
