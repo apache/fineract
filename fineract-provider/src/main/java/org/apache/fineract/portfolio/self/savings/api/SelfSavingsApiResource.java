424bd0884e7f21cc9c945fbbfd130be6423fbdbe
ShruthiRajaram <shruthi@confluxtechnologies.com>
2018-07-17 17:05:55 +0530

    FINERACT-628 Savings account APIs for Self Service App

--- fineract-provider/src/main/java/org/apache/fineract/portfolio/self/savings/api/SelfSavingsApiResource.java
+++ fineract-provider/src/main/java/org/apache/fineract/portfolio/self/savings/api/SelfSavingsApiResource.java
@@ -18,9 +18,13 @@
  */
 package org.apache.fineract.portfolio.self.savings.api;
 
+import java.util.HashMap;
+
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -30,10 +34,13 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.UriInfo;
 
 import org.apache.fineract.infrastructure.security.service.PlatformSecurityContext;
+import org.apache.fineract.portfolio.client.exception.ClientNotFoundException;
 import org.apache.fineract.portfolio.savings.api.SavingsAccountChargesApiResource;
 import org.apache.fineract.portfolio.savings.api.SavingsAccountTransactionsApiResource;
 import org.apache.fineract.portfolio.savings.api.SavingsAccountsApiResource;
 import org.apache.fineract.portfolio.savings.exception.SavingsAccountNotFoundException;
+import org.apache.fineract.portfolio.self.client.service.AppuserClientMapperReadService;
+import org.apache.fineract.portfolio.self.savings.data.SelfSavingsAccountConstants;
 import org.apache.fineract.portfolio.self.savings.data.SelfSavingsDataValidator;
 import org.apache.fineract.portfolio.self.savings.service.AppuserSavingsMapperReadService;
 import org.apache.fineract.useradministration.domain.AppUser;
@@ -52,6 +59,7 @@ public class SelfSavingsApiResource {
 	private final SavingsAccountTransactionsApiResource savingsAccountTransactionsApiResource;
 	private final AppuserSavingsMapperReadService appuserSavingsMapperReadService;
 	private final SelfSavingsDataValidator dataValidator;
+	private final AppuserClientMapperReadService appUserClientMapperReadService;
 
 	@Autowired
 	public SelfSavingsApiResource(
@@ -60,13 +68,15 @@ public class SelfSavingsApiResource {
 			final SavingsAccountChargesApiResource savingsAccountChargesApiResource,
 			final SavingsAccountTransactionsApiResource savingsAccountTransactionsApiResource,
 			final AppuserSavingsMapperReadService appuserSavingsMapperReadService,
-			final SelfSavingsDataValidator dataValidator) {
+			final SelfSavingsDataValidator dataValidator,
+			final AppuserClientMapperReadService appUserClientMapperReadService) {
 		this.context = context;
 		this.savingsAccountsApiResource = savingsAccountsApiResource;
 		this.savingsAccountChargesApiResource = savingsAccountChargesApiResource;
 		this.savingsAccountTransactionsApiResource = savingsAccountTransactionsApiResource;
 		this.appuserSavingsMapperReadService = appuserSavingsMapperReadService;
 		this.dataValidator = dataValidator;
+		this.appUserClientMapperReadService = appUserClientMapperReadService;
 	}
 
 	@GET
@@ -142,6 +152,53 @@ public class SelfSavingsApiResource {
 				.isSavingsMappedToUser(accountId, user.getId());
 		if (!isMappedSavings) {
 			throw new SavingsAccountNotFoundException(accountId);
+		}
+	}
+
+	@GET
+	@Path("template")
+	@Consumes({ MediaType.APPLICATION_JSON })
+	@Produces({ MediaType.APPLICATION_JSON })
+	public String template(@QueryParam("clientId") final Long clientId, @QueryParam("productId") final Long productId,
+			final String apiRequestBodyAsJson, @Context final UriInfo uriInfo) {
+
+		validateAppuserClientsMapping(clientId);
+		Long groupId = null;
+		boolean staffInSelectedOfficeOnly = false;
+		return this.savingsAccountsApiResource.template(clientId, groupId, productId, staffInSelectedOfficeOnly,
+				uriInfo);
+
+	}
+
+	@POST
+	@Consumes({ MediaType.APPLICATION_JSON })
+	@Produces({ MediaType.APPLICATION_JSON })
+	public String submitSavingsAccountApplication(@QueryParam("command") final String commandParam,
+			@Context final UriInfo uriInfo, final String apiRequestBodyAsJson) {
+
+		HashMap<String, Object> parameterMap = this.dataValidator.validateSavingsApplication(apiRequestBodyAsJson);
+		final Long clientId = (Long) parameterMap.get(SelfSavingsAccountConstants.clientIdParameterName);
+		validateAppuserClientsMapping(clientId);
+		return this.savingsAccountsApiResource.submitApplication(apiRequestBodyAsJson);
+	}
+
+	@PUT
+	@Path("{accountId}")
+	@Consumes({ MediaType.APPLICATION_JSON })
+	@Produces({ MediaType.APPLICATION_JSON })
+	public String modifySavingsAccountApplication(@PathParam("accountId") final Long accountId,
+			@QueryParam("command") final String commandParam, final String apiRequestBodyAsJson) {
+
+		validateAppuserSavingsAccountMapping(accountId);
+		this.dataValidator.validateSavingsApplication(apiRequestBodyAsJson);
+		return this.savingsAccountsApiResource.update(accountId, apiRequestBodyAsJson, commandParam);
+	}
+
+	private void validateAppuserClientsMapping(final Long clientId) {
+		AppUser user = this.context.authenticatedUser();
+		final boolean mappedClientId = this.appUserClientMapperReadService.isClientMappedToUser(clientId, user.getId());
+		if (!mappedClientId) {
+			throw new ClientNotFoundException(clientId);
 		}
 	}
 
