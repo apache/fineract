115aa4c6daac7df2ca38c564793f111812fd3237
Vishwas Babu A J <vishwas@confluxtechnologies.com>
2019-02-27 10:27:56 -0800

    Fixes for https://nvd.nist.gov/vuln/detail/CVE-2016-4977 and ignoring .idea files from RAT checks

--- fineract-provider/src/main/java/org/apache/fineract/infrastructure/cache/PlatformCacheConfiguration.java
+++ fineract-provider/src/main/java/org/apache/fineract/infrastructure/cache/PlatformCacheConfiguration.java
@@ -23,6 +23,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.CachingConfigurer;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.interceptor.CacheErrorHandler;
+import org.springframework.cache.interceptor.CacheResolver;
 import org.springframework.cache.interceptor.DefaultKeyGenerator;
 import org.springframework.cache.interceptor.KeyGenerator;
 import org.springframework.context.annotation.Bean;
@@ -43,7 +45,19 @@ public class PlatformCacheConfiguration implements CachingConfigurer {
     }
 
     @Override
+    public CacheResolver cacheResolver() {
+        //TODO https://issues.apache.org/jira/browse/FINERACT-705
+        return null;
+    }
+
+    @Override
     public KeyGenerator keyGenerator() {
         return new DefaultKeyGenerator();
+    }
+
+    @Override
+    public CacheErrorHandler errorHandler() {
+        //TODO https://issues.apache.org/jira/browse/FINERACT-705
+        return null;
     }
 }
