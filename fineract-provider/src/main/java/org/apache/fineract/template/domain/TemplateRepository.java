720a39d9d208d00382fff2722e3b9be482800de9
Michael Vorburger.ch <mike@vorburger.ch>
2019-06-14 02:53:13 +0200

    rename SQL Query Parameters (fixes FINERACT-766)

--- fineract-provider/src/main/java/org/apache/fineract/template/domain/TemplateRepository.java
+++ fineract-provider/src/main/java/org/apache/fineract/template/domain/TemplateRepository.java
@@ -24,5 +24,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TemplateRepository extends JpaRepository<Template, Long> {
 
-    List<Template> findByEntityAndType(TemplateEntity entity, TemplateType type);
+    List<Template> findByEntityAndType(TemplateEntity entity, TemplateType templateType);
 }
