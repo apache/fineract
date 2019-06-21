c45b2eccb1df4d1b3368374a7833554b283025aa
Kang Breder <kangbreder@gmail.com>
2019-06-21 18:06:18 +0100

    Added swagger document for Adhoc query and Search APIs

--- fineract-provider/src/main/java/org/apache/fineract/portfolio/search/api/SearchApiResourceSwagger.java
+++ fineract-provider/src/main/java/org/apache/fineract/portfolio/search/api/SearchApiResourceSwagger.java
@@ -17,59 +17,16 @@
  * under the License.
  */
 package org.apache.fineract.portfolio.search.api;
-import java.util.Collection;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import org.apache.fineract.infrastructure.core.data.EnumOptionData;
+import org.joda.time.LocalDate;
 import java.util.Set;
 
-import javax.ws.rs.Consumes;
-import javax.ws.rs.DefaultValue;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
-import javax.ws.rs.core.Context;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.UriInfo;
+import java.util.List;
 
-import org.apache.fineract.infrastructure.core.api.ApiRequestParameterHelper;
-import org.apache.fineract.infrastructure.core.serialization.ApiRequestJsonSerializationSettings;
-import org.apache.fineract.infrastructure.core.serialization.ToApiJsonSerializer;
-import org.apache.fineract.portfolio.search.SearchConstants.SEARCH_RESPONSE_PARAMETERS;
-import org.apache.fineract.portfolio.search.data.AdHocQueryDataValidator;
-import org.apache.fineract.portfolio.search.data.AdHocQuerySearchConditions;
-import org.apache.fineract.portfolio.search.data.AdHocSearchQueryData;
-import org.apache.fineract.portfolio.search.data.SearchConditions;
-import org.apache.fineract.portfolio.search.data.SearchData;
-import org.apache.fineract.portfolio.search.service.SearchReadPlatformService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Scope;
-import org.springframework.stereotype.Component;import java.util.Collection;
-import java.util.Set;
 
-import javax.ws.rs.Consumes;
-import javax.ws.rs.DefaultValue;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
-import javax.ws.rs.core.Context;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.UriInfo;
-
-import org.apache.fineract.infrastructure.core.api.ApiRequestParameterHelper;
-import org.apache.fineract.infrastructure.core.serialization.ApiRequestJsonSerializationSettings;
-import org.apache.fineract.infrastructure.core.serialization.ToApiJsonSerializer;
-import org.apache.fineract.portfolio.search.SearchConstants.SEARCH_RESPONSE_PARAMETERS;
-import org.apache.fineract.portfolio.search.data.AdHocQueryDataValidator;
-import org.apache.fineract.portfolio.search.data.AdHocQuerySearchConditions;
-import org.apache.fineract.portfolio.search.data.AdHocSearchQueryData;
-import org.apache.fineract.portfolio.search.data.SearchConditions;
-import org.apache.fineract.portfolio.search.data.SearchData;
-import org.apache.fineract.portfolio.search.service.SearchReadPlatformService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Scope;
-import org.springframework.stereotype.Component;
 
 /**
  * Created by Kang Breder on 13/06/19.
@@ -78,6 +35,7 @@ final class SearchApiResourceSwagger {
     private SearchApiResourceSwagger() {
 
     }
+
     @ApiModel(value = "GetSearchResponse")
     public static final class GetSearchResponse {
         private GetSearchResponse() {
@@ -103,7 +61,7 @@ final class SearchApiResourceSwagger {
 
     @ApiModel(value = "PostAdhocQuerySearchRequest")
     public static final class PostAdhocQuerySearchRequest {
-        private PostAdhocQuerySearchRequest(){
+        private PostAdhocQuerySearchRequest() {
 
         }
 
@@ -132,20 +90,22 @@ final class SearchApiResourceSwagger {
         public Long minOutstandingAmount;
         @ApiModelProperty(example = "10000")
         public Long maxOutstandingAmount;
-        }
+    }
 
     @ApiModel(value = "PostAdhocQuerySearchResponse")
     public static final class PostAdhocQuerySearchResponse {
         private PostAdhocQuerySearchResponse() {
             // don't allow to instantiate; use only for live API documentation
         }
-        ApiModelProperty(example = "HFC")
+
+        @ApiModelProperty(example = "HFC")
         public String officeName;
-        ApiModelProperty(example = "01 BC3M")
+        @ApiModelProperty(example = "01 BC3M")
         public String loanProductName;
         @ApiModelProperty(example = " 5692.41")
         public Long loanOutStanding;
         @ApiModelProperty(example = "76.4")
         public Long percentage;
     }
+}
 
