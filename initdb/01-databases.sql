2238cca76a8999d2241c41e0cd05a7c6e9c41b57
conradsp <sconrad1@gmail.com>
2019-06-28 08:49:06 -0500

    Added license, moved db init file

--- initdb/01-databases.sql
+++
@@ -1,6 +0,0 @@
-# create databases
-CREATE DATABASE IF NOT EXISTS `mifosplatform-tenants`;
-CREATE DATABASE IF NOT EXISTS `mifostenant-default`;
-
-# create root user and grant rights
-GRANT ALL ON *.* TO 'root'@'%';
