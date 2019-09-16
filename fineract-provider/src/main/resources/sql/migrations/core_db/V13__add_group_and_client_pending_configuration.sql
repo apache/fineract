15580f4cc23e9ca19a670da1a8da96170bf3a89c
Josue Araujo <jaraujo@metricas.mx>
2019-09-15 23:05:34 -0500

    * Added modifications to allow MySQL 8 compatibility (tested on MySQL 8.0.17)

--- fineract-provider/src/main/resources/sql/migrations/core_db/V13__add_group_and_client_pending_configuration.sql
+++ fineract-provider/src/main/resources/sql/migrations/core_db/V13__add_group_and_client_pending_configuration.sql
@@ -26,13 +26,13 @@ INSERT INTO c_configuration (`name`, `enabled`) VALUES ('allow-pending-client-st
 INSERT INTO c_configuration (`name`, `enabled`) VALUES ('allow-pending-group-status', '0');
 
 
-INSERT INTO .`r_enum_value` (`enum_name`, `enum_id`, `enum_message_property`, `enum_value`)
+INSERT INTO `r_enum_value` (`enum_name`, `enum_id`, `enum_message_property`, `enum_value`)
  VALUES ('status_id', '0', 'Invalid', 'Invalid');
-INSERT INTO .`r_enum_value` (`enum_name`, `enum_id`, `enum_message_property`, `enum_value`)
+INSERT INTO `r_enum_value` (`enum_name`, `enum_id`, `enum_message_property`, `enum_value`)
  VALUES ('status_id', '100', 'Pending', 'Pending');
-INSERT INTO .`r_enum_value` (`enum_name`, `enum_id`, `enum_message_property`, `enum_value`)
+INSERT INTO `r_enum_value` (`enum_name`, `enum_id`, `enum_message_property`, `enum_value`)
  VALUES ('status_id', '300', 'Active', 'Active');
-INSERT INTO .`r_enum_value` (`enum_name`, `enum_id`, `enum_message_property`, `enum_value`)
+INSERT INTO `r_enum_value` (`enum_name`, `enum_id`, `enum_message_property`, `enum_value`)
  VALUES ('status_id', '600', 'Closed', 'Closed');
-INSERT INTO .`r_enum_value` (`enum_name`, `enum_id`, `enum_message_property`, `enum_value`)
+INSERT INTO `r_enum_value` (`enum_name`, `enum_id`, `enum_message_property`, `enum_value`)
  VALUES ('loan_status_id', '0', 'Invalid', 'Invalid');
