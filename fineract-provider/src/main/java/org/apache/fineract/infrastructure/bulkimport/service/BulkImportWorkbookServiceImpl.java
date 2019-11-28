981bc17a606bf8dd33253937631c9825b42716fa
Maek Twain <saranshisat@icloud.com>
2019-11-28 08:03:17 +0530

    Adding vnd.ms-excel MIME type support in BulkImportWorkbookServiceImpl (#650)

--- fineract-provider/src/main/java/org/apache/fineract/infrastructure/bulkimport/service/BulkImportWorkbookServiceImpl.java
+++ fineract-provider/src/main/java/org/apache/fineract/infrastructure/bulkimport/service/BulkImportWorkbookServiceImpl.java
@@ -98,9 +98,12 @@ public class BulkImportWorkbookServiceImpl implements BulkImportWorkbookService 
                 final String fileType = tika.detect(tikaInputStream);
                 final String fileExtension = Files.getFileExtension(fileDetail.getFileName()).toLowerCase();
                 ImportFormatType format = ImportFormatType.of(fileExtension);
-                if (!fileType.contains("msoffice")) {
-                    throw new GeneralPlatformDomainRuleException("error.msg.invalid.file.extension",
-                            "Uploaded file extension is not recognized.");
+                if (!fileType.contains("msoffice") && !fileType.contains("application/vnd.ms-excel")) {
+                    // We had a problem where we tried to upload the downloaded file from the import options, it was somehow changed the
+                    // extension we use this fix.
+                        throw new GeneralPlatformDomainRuleException("error.msg.invalid.file.extension",
+                                "Uploaded file extension is not recognized.");
+                    
                 }
                 Workbook workbook = new HSSFWorkbook(clonedInputStreamWorkbook);
                 GlobalEntityType entityType=null;
