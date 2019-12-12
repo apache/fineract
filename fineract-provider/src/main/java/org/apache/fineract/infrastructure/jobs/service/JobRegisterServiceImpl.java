0da4cdd4121a411f86b2ea0241e5d880ad37645c
Kumaranath Fernando <wkk91193@yahoo.com>
2019-12-13 02:14:44 +0530

    Parallelization of Jobs: Parallelizing and paging of recalculate interest for loans (PR #524 for FINERACT-428)

--- fineract-provider/src/main/java/org/apache/fineract/infrastructure/jobs/service/JobRegisterServiceImpl.java
+++ fineract-provider/src/main/java/org/apache/fineract/infrastructure/jobs/service/JobRegisterServiceImpl.java
@@ -18,6 +18,7 @@
  */
 package org.apache.fineract.infrastructure.jobs.service;
 
+
 import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -25,7 +26,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Properties;
 import java.util.TimeZone;
-
+import java.util.Map;
 import javax.annotation.PostConstruct;
 
 import org.apache.fineract.infrastructure.core.domain.FineractPlatformTenant;
@@ -33,6 +34,8 @@ import org.apache.fineract.infrastructure.core.exception.PlatformInternalServerE
 import org.apache.fineract.infrastructure.core.service.ThreadLocalContextUtil;
 import org.apache.fineract.infrastructure.jobs.annotation.CronMethodParser;
 import org.apache.fineract.infrastructure.jobs.annotation.CronMethodParser.ClassMethodNamesPair;
+import org.apache.fineract.infrastructure.jobs.domain.JobParameter;
+import org.apache.fineract.infrastructure.jobs.domain.JobParameterRepository;
 import org.apache.fineract.infrastructure.jobs.domain.ScheduledJobDetail;
 import org.apache.fineract.infrastructure.jobs.domain.SchedulerDetail;
 import org.apache.fineract.infrastructure.jobs.exception.JobNotFoundException;
@@ -78,6 +81,7 @@ public class JobRegisterServiceImpl implements JobRegisterService, ApplicationLi
     private SchedulerJobListener schedulerJobListener;
     private SchedulerStopListener schedulerStopListener;
     private SchedulerTriggerListener globalSchedulerTriggerListener;
+    private JobParameterRepository jobParameterRepository;
 
     private final HashMap<String, Scheduler> schedulers = new HashMap<>(4);
 
@@ -109,6 +113,11 @@ public class JobRegisterServiceImpl implements JobRegisterService, ApplicationLi
     @Autowired
     public void setGlobalTriggerListener(SchedulerTriggerListener globalTriggerListener) {
         this.globalSchedulerTriggerListener = globalTriggerListener;
+    }
+
+    @Autowired
+    public  void setJobParameterRepository(JobParameterRepository jobParameterRepository){
+        this.jobParameterRepository=jobParameterRepository;
     }
 
     @PostConstruct
@@ -354,6 +363,14 @@ public class JobRegisterServiceImpl implements JobRegisterService, ApplicationLi
         return jobDetailFactoryBean.getObject();
     }
 
+    public Map<String,String> getJobParameter(ScheduledJobDetail scheduledJobDetail){
+        List<JobParameter> jobParameterList= jobParameterRepository.findJobParametersByJobId(scheduledJobDetail.getId());
+        Map<String,String> jobParameterMap=new HashMap<>();
+        for (JobParameter jobparameter:jobParameterList) {
+            jobParameterMap.put(jobparameter.getParameterName(),jobparameter.getParameterValue());
+        }
+        return  jobParameterMap;
+    }
     private Object getBeanObject(final Class<?> classType) throws ClassNotFoundException {
         final List<Class<?>> typesList = new ArrayList<>();
         final Class<?>[] interfaceType = classType.getInterfaces();
