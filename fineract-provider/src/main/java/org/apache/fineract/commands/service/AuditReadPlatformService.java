b2673948a6f608e251f3c12aa2af56fc6b27d198
Michael Vorburger <mike@vorburger.ch>
2020-03-08 18:17:16 +0100

    Fix audit trails filter (FINERACT-808)
    As per the great analysis by Manthan Surkar (@thesmallstar) in the
    original PR https://github.com/apache/fineract/pull/723, the cause of
    the FINERACT-808 bug was that "the backend would treat "UPDATE" and
    similar strings as SQL injection".
    
    The root cause of that was that (IMHO..) how Fineract does SQL injection
    is more of a workaround (blacklisting some keywords and some heuristic
    checks) then how this really should be done (by using JDBC Prepared
    statements with arguments for all external data, instead inlined SQL).
    
    This also lays the foundation for more like this in FINERACT-854.

--- fineract-provider/src/main/java/org/apache/fineract/commands/service/AuditReadPlatformService.java
+++ fineract-provider/src/main/java/org/apache/fineract/commands/service/AuditReadPlatformService.java
@@ -23,14 +23,15 @@ import org.apache.fineract.commands.data.AuditData;
 import org.apache.fineract.commands.data.AuditSearchData;
 import org.apache.fineract.infrastructure.core.data.PaginationParameters;
 import org.apache.fineract.infrastructure.core.service.Page;
+import org.apache.fineract.infrastructure.security.utils.SQLBuilder;
 
 public interface AuditReadPlatformService {
 
-    Collection<AuditData> retrieveAuditEntries(String extraCriteria, boolean includeJson);
+    Collection<AuditData> retrieveAuditEntries(SQLBuilder extraCriteria, boolean includeJson);
 
-    Page<AuditData> retrievePaginatedAuditEntries(String extraCriteria, boolean includeJson, PaginationParameters parameters);
+    Page<AuditData> retrievePaginatedAuditEntries(SQLBuilder extraCriteria, boolean includeJson, PaginationParameters parameters);
 
-    Collection<AuditData> retrieveAllEntriesToBeChecked(String extraCriteria, boolean includeJson);
+    Collection<AuditData> retrieveAllEntriesToBeChecked(SQLBuilder extraCriteria, boolean includeJson);
 
     AuditData retrieveAuditEntry(Long auditId);
 
