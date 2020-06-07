62fd8701ad3aa986c07138775a6837fb0a8c549f
percyashu <percyayukashu@gmail.com>
2020-06-08 00:14:27 +0100

    FINERACT-822 enable DoubleBraceInitialization error

--- fineract-provider/src/main/java/org/apache/fineract/infrastructure/security/exception/ResetPasswordException.java
+++ fineract-provider/src/main/java/org/apache/fineract/infrastructure/security/exception/ResetPasswordException.java
@@ -19,6 +19,7 @@
 package org.apache.fineract.infrastructure.security.exception;
 
 import java.util.ArrayList;
+import java.util.List;
 import org.apache.fineract.infrastructure.core.data.ApiParameterError;
 import org.apache.fineract.infrastructure.core.exception.PlatformApiDataValidationException;
 
@@ -31,14 +32,9 @@ public class ResetPasswordException extends PlatformApiDataValidationException {
     public ResetPasswordException(final Long userId) {
 
         super("error.msg.password.outdated", "The password of the user with id " + userId + " has expired, please reset it",
-                new ArrayList<ApiParameterError>() {
-
-                    {
-                        add(ApiParameterError.parameterError("error.msg.password.outdated", "The password of the user with id " + userId
-                                + " has expired, please reset it", "userId", userId));
-
-                    }
-                }
+                new ArrayList<ApiParameterError> (List.of(
+                        ApiParameterError.parameterError("error.msg.password.outdated", "The password of the user with id " + userId
+                                + " has expired, please reset it", "userId", userId)))
 
         );
 
