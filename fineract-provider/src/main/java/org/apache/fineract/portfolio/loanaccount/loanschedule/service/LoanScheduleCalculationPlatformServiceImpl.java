9542e357024335563f268b9600770bb8a3ffb331
percyashu <percyayukashu@gmail.com>
2020-06-22 17:10:25 +0100

    FINERACT-822 Enforce ModifiedButNotUsed check

--- fineract-provider/src/main/java/org/apache/fineract/portfolio/loanaccount/loanschedule/service/LoanScheduleCalculationPlatformServiceImpl.java
+++ fineract-provider/src/main/java/org/apache/fineract/portfolio/loanaccount/loanschedule/service/LoanScheduleCalculationPlatformServiceImpl.java
@@ -40,7 +40,6 @@ import org.apache.fineract.portfolio.loanaccount.domain.Loan;
 import org.apache.fineract.portfolio.loanaccount.domain.LoanDisbursementDetails;
 import org.apache.fineract.portfolio.loanaccount.domain.LoanRepaymentScheduleInstallment;
 import org.apache.fineract.portfolio.loanaccount.domain.LoanRepaymentScheduleTransactionProcessorFactory;
-import org.apache.fineract.portfolio.loanaccount.domain.LoanTransaction;
 import org.apache.fineract.portfolio.loanaccount.domain.transactionprocessor.LoanRepaymentScheduleTransactionProcessor;
 import org.apache.fineract.portfolio.loanaccount.loanschedule.data.LoanScheduleData;
 import org.apache.fineract.portfolio.loanaccount.loanschedule.data.LoanSchedulePeriodData;
@@ -183,16 +182,6 @@ public class LoanScheduleCalculationPlatformServiceImpl implements LoanScheduleC
             totalAmount = totalAmount.plus(interestDue);
         }
         boolean isNewPaymentRequired = loanRepaymentScheduleInstallment.isInterestDue(currency) || totalPrincipal.isGreaterThanZero();
-        final List<LoanTransaction> modifiedTransactions = new ArrayList<>();
-        List<LoanTransaction> transactions = loan.retreiveListOfTransactionsPostDisbursementExcludeAccruals();
-        for (LoanTransaction loanTransaction : transactions) {
-            modifiedTransactions.add(LoanTransaction.copyTransactionProperties(loanTransaction));
-        }
-        if (isNewPaymentRequired) {
-            LoanTransaction ondayPaymentTransaction = LoanTransaction.repayment(null, totalAmount, null, today, null,
-                    DateUtils.getLocalDateTimeOfTenant(), null);
-            modifiedTransactions.add(ondayPaymentTransaction);
-        }
 
         LoanScheduleModel model = this.loanScheduleAssembler.assembleForInterestRecalculation(loanApplicationTerms, loan.getOfficeId(),
                 loan, loanRepaymentScheduleTransactionProcessor, loan.fetchInterestRecalculateFromDate());
