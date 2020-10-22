578e098670c60eea7e865728cfd38d43c49d04f8
Aleksandar Vidakovic <aleks@apache.org>
2020-10-22 03:08:57 +0200

    FINERACT-1209: Integration Tests using new Swagger Client API

--- integration-tests/src/test/java/org/apache/fineract/integrationtests/guarantor/GuarantorTest.java
+++ integration-tests/src/test/java/org/apache/fineract/integrationtests/guarantor/GuarantorTest.java
@@ -16,7 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-package org.apache.fineract.integrationtests.loanaccount.guarantor;
+package org.apache.fineract.integrationtests.guarantor;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
