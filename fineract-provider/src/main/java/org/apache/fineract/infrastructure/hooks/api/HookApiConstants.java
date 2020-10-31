f4ada274bafcd84f82dd4987cd286a58fa69c934
Michael Vorburger <mike@vorburger.ch>
2020-10-31 14:23:53 +0100

    Revert "FINERACT-1241-elastic-web-hook (#1466)"
    This reverts commit 0a41cdc9d9564c586dc33b371318e1648adcd7d9.

--- fineract-provider/src/main/java/org/apache/fineract/infrastructure/hooks/api/HookApiConstants.java
+++ fineract-provider/src/main/java/org/apache/fineract/infrastructure/hooks/api/HookApiConstants.java
@@ -38,8 +38,6 @@ public final class HookApiConstants {
 
     public static final String webTemplateName = "Web";
 
-    public static final String elasticSearchTemplateName = "Elastic Search";
-
     public static final String smsTemplateName = "SMS Bridge";
 
     public static final String payloadURLName = "Payload URL";
