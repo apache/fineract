63e427333e063ece7a29f3beb4d03a202843f71f
Joseph Makara <joseph.irungu@strathmore.edu>
2021-03-29 11:55:37 +0300

    Fix Duplicate entry whilst issue (FINERACT-854)

--- fineract-provider/src/main/resources/sql/migrations/core_db/V365__reportCategoryList-FINERACT-1306.sql
+++ fineract-provider/src/main/resources/sql/migrations/core_db/V365__reportCategoryList-FINERACT-1306.sql
@@ -19,9 +19,9 @@
 
 -- two tables added: ReportCategoryList and FullReportList (FINERACT-1306)
 INSERT INTO stretchy_report (report_name, report_type, report_category, report_sql, description, core_report, use_report)
-SELECT "ReportCategoryList", 'Table', '(NULL)', '(NULL)', '(NULL)', 1, 1
+SELECT 'ReportCategoryList', 'Table', NULL, NULL, NULL, 1, 1
 ON DUPLICATE key UPDATE report_name = 'ReportCategoryList';
 
 INSERT INTO stretchy_report (report_name, report_type, report_category, report_sql, description, core_report, use_report)
-SELECT "FullReportList", 'Table', '(NULL)', '(NULL)', '(NULL)', 1, 1
+SELECT 'FullReportList', 'Table', NULL, NULL, NULL, 1, 1
 ON DUPLICATE key UPDATE report_name = 'FullReportList';
