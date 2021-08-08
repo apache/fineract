060089b6e84249bf96d267a03d21b15364e64b9b
SubhamPramanik <subhampramanik@gmail.com>
2021-08-08 17:37:36 +0530

    Added Collections Import

--- fineract-provider/src/main/java/org/apache/fineract/useradministration/service/AppUserWritePlatformServiceJpaRepositoryImpl.java
+++ fineract-provider/src/main/java/org/apache/fineract/useradministration/service/AppUserWritePlatformServiceJpaRepositoryImpl.java
@@ -21,6 +21,7 @@ package org.apache.fineract.useradministration.service;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
