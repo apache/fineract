61c8160e442f08a0a41d272885ab309bbd7f538f
BLasan <benuraab@gmail.com>
2021-08-23 16:01:37 +0530

    Roll back Import handler

--- integration-tests/src/test/java/org/apache/fineract/integrationtests/bulkimport/importhandler/loan/LoanImportHandlerTest.java
+++ integration-tests/src/test/java/org/apache/fineract/integrationtests/bulkimport/importhandler/loan/LoanImportHandlerTest.java
@@ -213,11 +213,7 @@ public class LoanImportHandlerTest {
         LOG.info("Output location: {}", location);
         LOG.info("Failure reason column: {}", row.getCell(LoanConstants.FAILURE_REPORT_COL).getStringCellValue());
 
-        /**
-         * TODO: For the repayment you'll need to add the post dated checks in disbursement. Change the output to
-         * `Imported` after proper validation.
-         */
-        Assertions.assertEquals("Repayment failed", row.getCell(LoanConstants.STATUS_COL).getStringCellValue());
+        Assertions.assertEquals("Imported", row.getCell(LoanConstants.STATUS_COL).getStringCellValue());
         outputworkbook.close();
     }
 }
