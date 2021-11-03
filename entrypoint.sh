f5c7339a8ce68920a9e429493fad1822ab9f73d7
phongnguyen-epay <phong.nguyen@epay.vn>
2021-11-03 09:01:01 +0000

    Use environment variable for dockerfile entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,11 +2,10 @@
 
 set -e
 
-while ! nc -zvw3 fineractmysql 3306 ; do
+while ! nc -zvw3 $FINERACT_DEFAULT_TENANTDB_HOSTNAME $FINERACT_DEFAULT_TENANTDB_PORT ; do
     >&2 echo "DB Server is unavailable - sleeping"
     sleep 5
 done
 >&2 echo "DB Server is up - executing command"
 
 java -Dloader.path=/app/libs/ -jar /app/fineract-provider.jar
-
