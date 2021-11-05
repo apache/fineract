fee3d734261c53991277d76f51a7d2178d778ba7
Petri Tuomola <petri@tuomola.org>
2021-11-05 16:14:05 +0800

    Fix test broken by PR1917

--- fineract-provider/src/main/resources/sql/migrations/core_db/V374__adding_expenseliablity_to_paymentorfeetype.sql
+++ fineract-provider/src/main/resources/sql/migrations/core_db/V374__adding_expenseliablity_to_paymentorfeetype.sql
@@ -20,6 +20,6 @@
 ALTER TABLE `c_configuration`
     ADD COLUMN `string_value` VARCHAR(100) NULL DEFAULT NULL AFTER `date_value`;
 
-insert into c_configuration(name, string_value, enabled, description) values('account-mapping-for-payment-type', 'Asset', '1', 'Asset: default for asset, Use comma seperated values for Liability, Asset and Expense accounts');
+insert into c_configuration(name, string_value, enabled, description) values('account-mapping-for-payment-type', 'Asset', 0, 'Asset: default for asset, Use comma seperated values for Liability, Asset and Expense accounts');
 
-insert into c_configuration(name, string_value, enabled, description) values('account-mapping-for-charge', 'Income', '1', 'Income: default for Income, Use comma seperated values for Income, Liability and Expense accounts');
+insert into c_configuration(name, string_value, enabled, description) values('account-mapping-for-charge', 'Income', 0, 'Income: default for Income, Use comma seperated values for Income, Liability and Expense accounts');
