3f96bf05b0d619003425da143d5c796ded4d51a7
sheminanto <sheminanto@gmail.com>
2021-11-29 17:57:20 +0530

    FINERACT-1447 : Fix - Overdraft interest calculated is wrong

--- fineract-provider/src/main/java/org/apache/fineract/portfolio/savings/domain/SavingsAccountTransaction.java
+++ fineract-provider/src/main/java/org/apache/fineract/portfolio/savings/domain/SavingsAccountTransaction.java
@@ -571,7 +571,7 @@ public final class SavingsAccountTransaction extends AbstractPersistableCustom {
             endOfDayBalance = openingBalance.plus(getAmount(currency));
         } else if (isWithdrawal() || isChargeTransactionAndNotReversed()) {
 
-            if (openingBalance.isGreaterThanZero()) {
+            if (openingBalance.isGreaterThanZero() || this.savingsAccount.allowOverdraft()) {
                 endOfDayBalance = openingBalance.minus(getAmount(currency));
             } else {
                 endOfDayBalance = Money.of(currency, this.runningBalance);
@@ -603,7 +603,7 @@ public final class SavingsAccountTransaction extends AbstractPersistableCustom {
                 // }
             } else if (isWithdrawal() || isChargeTransactionAndNotReversed()) {
                 // endOfDayBalance = openingBalance.minus(getAmount(currency));
-                if (endOfDayBalance.isGreaterThanZero()) {
+                if (endOfDayBalance.isGreaterThanZero() || this.savingsAccount.allowOverdraft()) {
                     endOfDayBalance = endOfDayBalance.minus(getAmount(currency));
                 } else {
                     endOfDayBalance = Money.of(currency, this.runningBalance);
