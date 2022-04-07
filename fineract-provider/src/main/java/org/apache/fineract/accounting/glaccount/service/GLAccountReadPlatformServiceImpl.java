94e3b5e6b4343a1ea2a86cffcb95f5498aa19629
Jose Alberto Hernandez <alberto@MacBook-Pro.local>
2022-04-07 11:56:47 -0500

    Change smallint to booleans in SQL sentences

--- fineract-provider/src/main/java/org/apache/fineract/accounting/glaccount/service/GLAccountReadPlatformServiceImpl.java
+++ fineract-provider/src/main/java/org/apache/fineract/accounting/glaccount/service/GLAccountReadPlatformServiceImpl.java
@@ -147,8 +147,8 @@ public class GLAccountReadPlatformServiceImpl implements GLAccountReadPlatformSe
         if (filtersPresent) {
             boolean firstWhereConditionAdded = false;
             if (accountClassification != null) {
-                sql += " classification_enum like ?";
-                paramaterArray[arrayPos] = accountClassification;
+                sql += " classification_enum = ?";
+                paramaterArray[arrayPos] = accountClassification.shortValue();
                 arrayPos = arrayPos + 1;
                 firstWhereConditionAdded = true;
             }
@@ -179,11 +179,7 @@ public class GLAccountReadPlatformServiceImpl implements GLAccountReadPlatformSe
                     sql += " and ";
                 }
 
-                if (manualTransactionsAllowed) {
-                    sql += " manual_journal_entries_allowed = 1";
-                } else {
-                    sql += " manual_journal_entries_allowed = 0";
-                }
+                sql += " manual_journal_entries_allowed = " + manualTransactionsAllowed;
                 firstWhereConditionAdded = true;
             }
             if (disabled != null) {
@@ -191,11 +187,8 @@ public class GLAccountReadPlatformServiceImpl implements GLAccountReadPlatformSe
                     sql += " and ";
                 }
 
-                if (disabled) {
-                    sql += " disabled = 1";
-                } else {
-                    sql += " disabled = 0";
-                }
+                sql += " disabled = " + disabled;
+
                 firstWhereConditionAdded = true;
             }
         }
