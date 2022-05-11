66621dcc819ceaf988049085020cd10bd860ecdd
John Woodlock <john.woodlock@gmail.com>
2022-05-12 00:56:01 +0100

    FINERACT-1618 Cater for loans with no schedule at disburse time

--- fineract-provider/src/main/java/org/apache/fineract/portfolio/loanaccount/handler/LoanApplicationDeletionCommandHandler.java
+++ fineract-provider/src/main/java/org/apache/fineract/portfolio/loanaccount/handler/LoanApplicationDeletionCommandHandler.java
@@ -18,25 +18,21 @@
  */
 package org.apache.fineract.portfolio.loanaccount.handler;
 
+import lombok.RequiredArgsConstructor;
 import org.apache.fineract.commands.annotation.CommandType;
 import org.apache.fineract.commands.handler.NewCommandSourceHandler;
 import org.apache.fineract.infrastructure.core.api.JsonCommand;
 import org.apache.fineract.infrastructure.core.data.CommandProcessingResult;
 import org.apache.fineract.portfolio.loanaccount.service.LoanApplicationWritePlatformService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@RequiredArgsConstructor
 @CommandType(entity = "LOAN", action = "DELETE")
 public class LoanApplicationDeletionCommandHandler implements NewCommandSourceHandler {
 
     private final LoanApplicationWritePlatformService writePlatformService;
-
-    @Autowired
-    public LoanApplicationDeletionCommandHandler(final LoanApplicationWritePlatformService writePlatformService) {
-        this.writePlatformService = writePlatformService;
-    }
 
     @Transactional
     @Override
