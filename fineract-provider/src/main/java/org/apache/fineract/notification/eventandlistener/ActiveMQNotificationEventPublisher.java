6962a309610d8dd6eba54a34faccac2aa3df1f2e
Jose Alberto Hernandez <alberto@MacBook-Pro.local>
2022-05-28 15:43:08 -0500

    Enable/Disable Messaging in Read Only instance mode

--- fineract-provider/src/main/java/org/apache/fineract/notification/eventandlistener/ActiveMQNotificationEventPublisher.java
+++ fineract-provider/src/main/java/org/apache/fineract/notification/eventandlistener/ActiveMQNotificationEventPublisher.java
@@ -21,13 +21,16 @@ package org.apache.fineract.notification.eventandlistener;
 import javax.jms.Queue;
 import lombok.RequiredArgsConstructor;
 import org.apache.activemq.command.ActiveMQQueue;
+import org.apache.fineract.infrastructure.core.config.EnableFineractEventsCondition;
 import org.apache.fineract.notification.data.NotificationData;
+import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Profile;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 @Profile("activeMqEnabled")
+@Conditional(EnableFineractEventsCondition.class)
 @RequiredArgsConstructor
 public class ActiveMQNotificationEventPublisher implements NotificationEventPublisher {
 
