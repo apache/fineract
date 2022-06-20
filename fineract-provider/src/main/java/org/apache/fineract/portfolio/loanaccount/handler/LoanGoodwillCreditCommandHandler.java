f94a4bae138b560641304893562a45f10297ed13
John Woodlock <john.woodlock@gmail.com>
2022-06-20 11:46:54 +0100

    FINERACT-1643 Catch external_id duplicates for loan transactions

--- fineract-provider/src/main/java/org/apache/fineract/portfolio/loanaccount/handler/LoanGoodwillCreditCommandHandler.java
+++ fineract-provider/src/main/java/org/apache/fineract/portfolio/loanaccount/handler/LoanGoodwillCreditCommandHandler.java
@@ -21,25 +21,35 @@ package org.apache.fineract.portfolio.loanaccount.handler;
 import lombok.RequiredArgsConstructor;
 import org.apache.fineract.commands.annotation.CommandType;
 import org.apache.fineract.commands.handler.NewCommandSourceHandler;
+import org.apache.fineract.infrastructure.DataIntegrityErrorHandler;
 import org.apache.fineract.infrastructure.core.api.JsonCommand;
 import org.apache.fineract.infrastructure.core.data.CommandProcessingResult;
 import org.apache.fineract.portfolio.loanaccount.domain.LoanTransactionType;
 import org.apache.fineract.portfolio.loanaccount.service.LoanWritePlatformService;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.orm.jpa.JpaSystemException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 @CommandType(entity = "LOAN", action = "GOODWILLCREDIT")
-public class LoanGoodwilCreditCommandHandler implements NewCommandSourceHandler {
+public class LoanGoodwillCreditCommandHandler implements NewCommandSourceHandler {
 
     private final LoanWritePlatformService writePlatformService;
+    private final DataIntegrityErrorHandler dataIntegrityErrorHandler;
 
     @Transactional
     @Override
     public CommandProcessingResult processCommand(final JsonCommand command) {
-        boolean isRecoveryRepayment = false;
-        return this.writePlatformService.makeLoanRepayment(LoanTransactionType.GOODWILL_CREDIT, command.getLoanId(), command,
-                isRecoveryRepayment);
+        try {
+            boolean isRecoveryRepayment = false;
+            return this.writePlatformService.makeLoanRepayment(LoanTransactionType.GOODWILL_CREDIT, command.getLoanId(), command,
+                    isRecoveryRepayment);
+        } catch (final JpaSystemException | DataIntegrityViolationException dve) {
+            dataIntegrityErrorHandler.handleDataIntegrityIssues(command, dve.getMostSpecificCause(), dve, "loan.goodwillCredit",
+                    "Goodwill Credit");
+            return CommandProcessingResult.empty();
+        }
     }
 }
