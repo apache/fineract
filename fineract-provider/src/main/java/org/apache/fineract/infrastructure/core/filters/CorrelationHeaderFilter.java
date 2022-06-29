d286ac0b8d100abc4c7528ed17a7f77be70b3d5d
Vic Romero <victor.romero@fintecheando.mx>
2022-06-29 03:53:36 -0500

    FINERACT-1656 Correlation ID propagation and configuration

---
+++ fineract-provider/src/main/java/org/apache/fineract/infrastructure/core/filters/CorrelationHeaderFilter.java
@@ -0,0 +1,77 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.fineract.infrastructure.core.filters;
+
+import java.io.IOException;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.fineract.infrastructure.security.utils.LogParameterEscapeUtil;
+import org.slf4j.MDC;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@RequiredArgsConstructor
+@Slf4j
+public class CorrelationHeaderFilter extends OncePerRequestFilter {
+
+    private String correlationIdHeader;
+
+    public static final String correlationIdKey = "correlationId";
+
+    @Autowired
+    public CorrelationHeaderFilter(Environment env) {
+        correlationIdHeader = env.getRequiredProperty("fineract.logging.http.correlation-id.header-name");
+    }
+
+    @Override
+    protected void doFilterInternal(final HttpServletRequest request, final HttpServletResponse response, final FilterChain filterChain)
+            throws IOException, ServletException {
+
+        try {
+            final HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+            String currentCorrId = httpServletRequest.getHeader(correlationIdHeader);
+            log.debug("Found correlationId in Header : {}", LogParameterEscapeUtil.escapeLogMDCParameter(currentCorrId));
+            MDC.put(correlationIdKey, currentCorrId);
+            filterChain.doFilter(request, response);
+        } finally {
+            MDC.remove(correlationIdKey);
+        }
+    }
+
+    public static String getCurrentValue() {
+        return MDC.get(correlationIdKey);
+    }
+
+    @Override
+    protected boolean isAsyncDispatch(final HttpServletRequest request) {
+        return false;
+    }
+
+    @Override
+    protected boolean shouldNotFilterErrorDispatch() {
+        return false;
+    }
+
+}
