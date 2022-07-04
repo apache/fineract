e90dd49c819b3dcf94e945f8f4f6bcb619bf595a
logoutdhaval <dhaval@fynarfin.io>
2022-07-04 21:55:27 +0530

    FINERACT-1541: Pentaho reports converted to table format

--- integration-tests/src/test/java/org/apache/fineract/integrationtests/client/ReportsTest.java
+++ integration-tests/src/test/java/org/apache/fineract/integrationtests/client/ReportsTest.java
@@ -23,6 +23,8 @@ import java.util.Map;
 import okhttp3.MediaType;
 import okhttp3.Request;
 import okhttp3.ResponseBody;
+import org.apache.fineract.integrationtests.common.Utils;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
@@ -33,9 +35,14 @@ import org.junit.jupiter.api.Test;
  */
 public class ReportsTest extends IntegrationTest {
 
+    @BeforeEach
+    public void setup() {
+        Utils.initializeRESTAssured();
+    }
+
     @Test
     void listReports() {
-        assertThat(ok(fineract().reports.retrieveReportList())).hasSize(120);
+        assertThat(ok(fineract().reports.retrieveReportList())).hasSize(124);
     }
 
     @Test
@@ -68,5 +75,31 @@ public class ReportsTest extends IntegrationTest {
         ResponseBody r = ok(fineract().reportsRun.runReportGetFile("Expected Payments By Date - Formatted", Map.of("R_endDate",
                 "2013-04-30", "R_loanOfficerId", "-1", "R_officeId", "1", "R_startDate", "2013-04-16", "output-type", "PDF"), false));
         assertThat(r.contentType()).isEqualTo(MediaType.get("application/pdf"));
+    }
+
+    @Test
+    void testTrialBalanceTableReportRunsSuccessfully() {
+        assertThat(fineract().reportsRun.runReportGetData("Trial Balance Table",
+                Map.of("R_endDate", "2013-04-30", "R_officeId", "1", "R_startDate", "2013-04-16"), false)).hasHttpStatus(200);
+    }
+
+    @Test
+    void testIncomeStatementTableReportRunsSuccessfully() {
+        assertThat(fineract().reportsRun.runReportGetData("Income Statement Table",
+                Map.of("R_endDate", "2013-04-30", "R_officeId", "1", "R_startDate", "2013-04-16"), false)).hasHttpStatus(200);
+    }
+
+    @Test
+    void testGeneralLedgerReportTableReportRunsSuccessfully() {
+        assertThat(fineract().reportsRun.runReportGetData("GeneralLedgerReport Table",
+                Map.of("R_endDate", "2013-04-30", "R_officeId", "1", "R_startDate", "2013-04-16", "R_GLAccountNO", "1"), false))
+                        .hasHttpStatus(200);
+    }
+
+    @Test
+    void testBalanceSheetTableReportRunsSuccessfully() {
+        assertThat(
+                fineract().reportsRun.runReportGetData("Balance Sheet Table", Map.of("R_endDate", "2013-04-30", "R_officeId", "1"), false))
+                        .hasHttpStatus(200);
     }
 }
