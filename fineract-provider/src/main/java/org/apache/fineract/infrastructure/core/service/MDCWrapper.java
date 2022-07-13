4f832f94810d8b5aae321390f4074e6007c8d7db
Arnold Galovics <galovicsarnold@gmail.com>
2022-07-13 17:55:04 +0200

    Correlation ID support implementation

--- fineract-provider/src/main/java/org/apache/fineract/infrastructure/core/service/MDCWrapper.java
+++ fineract-provider/src/main/java/org/apache/fineract/infrastructure/core/service/MDCWrapper.java
@@ -19,39 +19,17 @@
 
 package org.apache.fineract.infrastructure.core.service;
 
-import java.util.Map;
 import org.slf4j.MDC;
-import org.slf4j.spi.MDCAdapter;
+import org.springframework.stereotype.Component;
 
-public class MdcAdapter implements MDCAdapter {
+@Component
+public class MDCWrapper {
 
-    @Override
     public void put(String key, String val) {
         MDC.put(key, val);
     }
 
-    @Override
-    public String get(String key) {
-        return MDC.get(key);
-    }
-
-    @Override
     public void remove(String key) {
         MDC.remove(key);
-    }
-
-    @Override
-    public void clear() {
-        MDC.clear();
-    }
-
-    @Override
-    public Map<String, String> getCopyOfContextMap() {
-        return MDC.getCopyOfContextMap();
-    }
-
-    @Override
-    public void setContextMap(Map<String, String> map) {
-        MDC.setContextMap(map);
     }
 }
