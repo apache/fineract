fdfc7062ff14f87d0f55328501a59999d67bc331
Jose Alberto Hernandez <alberto@MacBook-Pro.local>
2022-07-27 15:18:51 -0500

    Fix Product Mix queries using group by

---
+++ integration-tests/src/test/java/org/apache/fineract/integrationtests/ProductMixIntegrationTest.java
@@ -0,0 +1,59 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.fineract.integrationtests;
+
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+import java.util.ArrayList;
+import java.util.HashMap;
+import org.apache.fineract.integrationtests.common.ProductMixHelper;
+import org.apache.fineract.integrationtests.common.Utils;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class ProductMixIntegrationTest {
+
+    private RequestSpecification requestSpec;
+    private ResponseSpecification responseSpec;
+
+    private ProductMixHelper productMixHelper;
+
+    @BeforeEach
+    public void setUp() throws Exception {
+        Utils.initializeRESTAssured();
+        this.requestSpec = new RequestSpecBuilder().setContentType(ContentType.JSON).build();
+        this.requestSpec.header("Authorization", "Basic " + Utils.loginIntoServerAndGetBase64EncodedAuthenticationKey());
+        this.responseSpec = new ResponseSpecBuilder().expectStatusCode(200).build();
+        this.productMixHelper = new ProductMixHelper(this.requestSpec, this.responseSpec);
+    }
+
+    @Test
+    public void getProductMixList() {
+        final ArrayList productMixList = this.productMixHelper.getProductsMixList();
+        Assertions.assertNotNull(productMixList);
+
+        final HashMap productMixTemplate = (HashMap) this.productMixHelper.getProductMixTemplate();
+        Assertions.assertNotNull(productMixTemplate);
+    }
+
+}
