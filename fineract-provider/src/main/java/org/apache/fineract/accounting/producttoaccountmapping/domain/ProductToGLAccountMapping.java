78c541e992b8eefc9e931dce502d21d8695847cc
Hemant Gupta <gupta.hemant65@gmail.com>
2022-08-29 03:51:39 +0530

    refactor: add lombok to producttoaccountmapping module

--- fineract-provider/src/main/java/org/apache/fineract/accounting/producttoaccountmapping/domain/ProductToGLAccountMapping.java
+++ fineract-provider/src/main/java/org/apache/fineract/accounting/producttoaccountmapping/domain/ProductToGLAccountMapping.java
@@ -24,11 +24,19 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.Accessors;
 import org.apache.fineract.accounting.glaccount.domain.GLAccount;
 import org.apache.fineract.infrastructure.core.domain.AbstractPersistableCustom;
 import org.apache.fineract.portfolio.charge.domain.Charge;
 import org.apache.fineract.portfolio.paymenttype.domain.PaymentType;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@Accessors(chain = true)
 @Entity
 @Table(name = "acc_product_mapping", uniqueConstraints = { @UniqueConstraint(columnNames = { "product_id", "product_type",
         "financial_account_type", "payment_type" }, name = "financial_action") })
@@ -57,84 +65,9 @@ public class ProductToGLAccountMapping extends AbstractPersistableCustom {
 
     public static ProductToGLAccountMapping createNew(final GLAccount glAccount, final Long productId, final int productType,
             final int financialAccountType) {
-        return new ProductToGLAccountMapping(glAccount, productId, productType, financialAccountType);
-    }
 
-    protected ProductToGLAccountMapping() {
-        //
-    }
-
-    public ProductToGLAccountMapping(final GLAccount glAccount, final Long productId, final int productType,
-            final int financialAccountType) {
-        this(glAccount, productId, productType, financialAccountType, null, null);
-    }
-
-    public ProductToGLAccountMapping(final GLAccount glAccount, final Long productId, final int productType, final int financialAccountType,
-            final Charge charge) {
-        this(glAccount, productId, productType, financialAccountType, null, charge);
-    }
-
-    public ProductToGLAccountMapping(final GLAccount glAccount, final Long productId, final int productType, final int financialAccountType,
-            final PaymentType paymentType) {
-        this(glAccount, productId, productType, financialAccountType, paymentType, null);
-    }
-
-    private ProductToGLAccountMapping(final GLAccount glAccount, final Long productId, final int productType,
-            final int financialAccountType, final PaymentType paymentType, final Charge charge) {
-        this.glAccount = glAccount;
-        this.productId = productId;
-        this.productType = productType;
-        this.financialAccountType = financialAccountType;
-        this.paymentType = paymentType;
-        this.charge = charge;
-    }
-
-    public GLAccount getGlAccount() {
-        return this.glAccount;
-    }
-
-    public void setGlAccount(final GLAccount glAccount) {
-        this.glAccount = glAccount;
-    }
-
-    public Long getProductId() {
-        return this.productId;
-    }
-
-    public void setProductId(final Long productId) {
-        this.productId = productId;
-    }
-
-    public int getProductType() {
-        return this.productType;
-    }
-
-    public void setProductType(final int productType) {
-        this.productType = productType;
-    }
-
-    public int getFinancialAccountType() {
-        return this.financialAccountType;
-    }
-
-    public void setFinancialAccountType(final int financialAccountType) {
-        this.financialAccountType = financialAccountType;
-    }
-
-    public PaymentType getPaymentType() {
-        return this.paymentType;
-    }
-
-    public void setPaymentType(final PaymentType paymentType) {
-        this.paymentType = paymentType;
-    }
-
-    public Charge getCharge() {
-        return this.charge;
-    }
-
-    public void setCharge(final Charge charge) {
-        this.charge = charge;
+        return new ProductToGLAccountMapping().setGlAccount(glAccount).setProductId(productId).setProductType(productType)
+                .setFinancialAccountType(financialAccountType);
     }
 
 }
