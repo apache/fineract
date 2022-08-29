aa12de397dc0100da7e13346e625f7a453e2e644
Hemant Gupta <gupta.hemant65@gmail.com>
2022-08-30 02:52:53 +0530

    refactor: add lombok to provisioning module

--- fineract-provider/src/main/java/org/apache/fineract/accounting/provisioning/data/ProvisioningEntryData.java
+++ fineract-provider/src/main/java/org/apache/fineract/accounting/provisioning/data/ProvisioningEntryData.java
@@ -21,7 +21,13 @@ package org.apache.fineract.accounting.provisioning.data;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.Collection;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
 
+@Data
+@NoArgsConstructor
+@Accessors(chain = true)
 @SuppressWarnings("unused")
 public class ProvisioningEntryData {
 
@@ -43,32 +49,4 @@ public class ProvisioningEntryData {
 
     private Collection<LoanProductProvisioningEntryData> provisioningEntries;
 
-    public ProvisioningEntryData(final Long id, final Collection<LoanProductProvisioningEntryData> provisioningEntries) {
-        this.provisioningEntries = provisioningEntries;
-        this.id = id;
-    }
-
-    public ProvisioningEntryData(Long id, Boolean journalEntry, Long createdById, String createdUser, LocalDate createdDate,
-            Long modifiedById, String modifiedUser, BigDecimal totalReservedAmount) {
-        this.id = id;
-        this.journalEntry = journalEntry;
-        this.createdById = createdById;
-        this.createdUser = createdUser;
-        this.modifiedById = modifiedById;
-        this.modifiedUser = modifiedUser;
-        this.createdDate = createdDate;
-        this.reservedAmount = totalReservedAmount;
-    }
-
-    public void setEntries(Collection<LoanProductProvisioningEntryData> provisioningEntries) {
-        this.provisioningEntries = provisioningEntries;
-    }
-
-    public Long getId() {
-        return this.id;
-    }
-
-    public LocalDate getCreatedDate() {
-        return this.createdDate;
-    }
 }
