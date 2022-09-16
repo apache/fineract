4e2ec3d8251cdc11fc60384070611ec3b15d42d5
logoutdhaval <dhaval@fynarfin.io>
2022-09-16 20:39:03 +0530

    FINERACT-1748: Loan prepayment issue after advance payment

--- integration-tests/src/test/java/org/apache/fineract/integrationtests/common/loans/LoanProductTestBuilder.java
+++ integration-tests/src/test/java/org/apache/fineract/integrationtests/common/loans/LoanProductTestBuilder.java
@@ -130,6 +130,7 @@ public class LoanProductTestBuilder {
     private boolean syncExpectedWithDisbursementDate = false;
     private String fixedPrincipalPercentagePerInstallment;
     private String installmentAmountInMultiplesOf;
+    private boolean canDefineInstallmentAmount;
 
     public String build(final String chargeId) {
         final HashMap<String, Object> map = build(chargeId, null);
@@ -183,7 +184,9 @@ public class LoanProductTestBuilder {
                 map.put("overAppliedNumber", this.overAppliedNumber);
             }
         }
-
+        if (this.canDefineInstallmentAmount) {
+            map.put("canDefineInstallmentAmount", this.canDefineInstallmentAmount);
+        }
         if (multiDisburseLoan) {
             map.put("multiDisburseLoan", this.multiDisburseLoan);
             map.put("maxTrancheCount", this.maxTrancheCount);
@@ -463,6 +466,11 @@ public class LoanProductTestBuilder {
     public LoanProductTestBuilder withAccounting(final String accountingRule, final Account[] account_list) {
         this.accountingRule = accountingRule;
         this.accountList = account_list;
+        return this;
+    }
+
+    public LoanProductTestBuilder withDefineInstallmentAmount(final boolean canDefineInstallmentAmount) {
+        this.canDefineInstallmentAmount = canDefineInstallmentAmount;
         return this;
     }
 
