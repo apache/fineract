5209b19b4c720db47c13a5017a28ba1085bef0dd
Mayank Gupta <mayankgupta@indiagold.co>
2023-01-23 12:41:17 +0400

    fix

--- fineract-provider/src/main/java/org/apache/fineract/useradministration/domain/AppUser.java
+++ fineract-provider/src/main/java/org/apache/fineract/useradministration/domain/AppUser.java
@@ -124,8 +124,7 @@ public class AppUser extends AbstractPersistableCustom implements PlatformUser {
 
         final String username = command.stringValueOfParameterNamed("username");
         String password = command.stringValueOfParameterNamed("password");
-//        final Boolean sendPasswordToEmail = command.booleanObjectValueOfParameterNamed("sendPasswordToEmail");
-        final Boolean sendPasswordToEmail = Boolean.FALSE;
+        final Boolean sendPasswordToEmail = command.booleanObjectValueOfParameterNamed("sendPasswordToEmail");
 
         if (sendPasswordToEmail) {
             password = new RandomPasswordGenerator(13).generate();
