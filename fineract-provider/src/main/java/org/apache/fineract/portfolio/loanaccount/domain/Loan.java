5872e14086ba711085ec573943a48d1b1de2f5a9
Arnold Gálovics <galovics@users.noreply.github.com>
2023-01-25 12:08:57 +0100

    Revert "Fix to use only loan transaction type 2 in getLastRepaymentTransaction"
    This reverts commit e1e3ba2c10679ccb94b7fee2710fa14acdb90c3d.

--- fineract-provider/src/main/java/org/apache/fineract/portfolio/loanaccount/domain/Loan.java
+++ fineract-provider/src/main/java/org/apache/fineract/portfolio/loanaccount/domain/Loan.java
@@ -4946,7 +4946,7 @@ public class Loan extends AbstractAuditableWithUTCDateTimeCustom {
 
     public LoanTransaction getLastRepaymentTransaction() {
         for (final LoanTransaction loanTransaction : this.loanTransactions) {
-            if (!loanTransaction.isReversed() && loanTransaction.isRepayment()) {
+            if (!loanTransaction.isReversed() && loanTransaction.isRepaymentType()) {
                 return loanTransaction;
             }
         }
