fe5d7cccee63e1f64e1fd74ac5cdbbaf8a5d914e
Mkkor <muthu@korconnection.com>
2023-07-20 18:31:16 +0530

    FINERACT-1943 Add SubmittedOnDate to Savings Transaction

--- fineract-provider/src/main/java/org/apache/fineract/portfolio/savings/service/SavingsSchedularInterestPoster.java
+++ fineract-provider/src/main/java/org/apache/fineract/portfolio/savings/service/SavingsSchedularInterestPoster.java
@@ -206,7 +206,8 @@ public class SavingsSchedularInterestPoster {
                             savingsAccountTransactionData.getBalanceNumberOfDays(), savingsAccountTransactionData.getRunningBalance(),
                             savingsAccountTransactionData.getCumulativeBalance(), currentDate, Integer.valueOf(1),
                             savingsAccountTransactionData.isManualTransaction(), savingsAccountTransactionData.getRefNo(),
-                            savingsAccountTransactionData.isReversalTransaction(), savingsAccountTransactionData.getOverdraftAmount(), });
+                            savingsAccountTransactionData.isReversalTransaction(), savingsAccountTransactionData.getOverdraftAmount(),
+                            DateUtils.getBusinessLocalDate() });
                 } else {
                     paramsForTransactionUpdate.add(new Object[] { savingsAccountTransactionData.isReversed(),
                             savingsAccountTransactionData.getAmount(), savingsAccountTransactionData.getOverdraftAmount(), balanceEndDate,
@@ -244,8 +245,8 @@ public class SavingsSchedularInterestPoster {
         query.append("transaction_type_enum, transaction_date, amount, balance_end_date_derived, ");
         query.append("balance_number_of_days_derived, running_balance_derived, cumulative_balance_derived, ");
         query.append("created_date, appuser_id, is_manual, ref_no, is_reversal, ");
-        query.append("overdraft_amount_derived) VALUES ");
-        query.append("(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+        query.append("overdraft_amount_derived, submitted_on_date) VALUES ");
+        query.append("(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
         return query.toString();
 
     }
