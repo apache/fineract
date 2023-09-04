19bd4fcb29ae2adc2f40d14c818ac3c320fdb2ac
Mohamed Magdi-Abdelmonem <mohamed.magdi.abdelmonem@gmail.com>
2023-09-04 22:16:13 +0300

    FINERACT-1932: Add custom Configuration class for self-client module

--- fineract-provider/src/main/java/org/apache/fineract/portfolio/self/client/service/AppuserClientMapperReadServiceImpl.java
+++ fineract-provider/src/main/java/org/apache/fineract/portfolio/self/client/service/AppuserClientMapperReadServiceImpl.java
@@ -18,24 +18,17 @@
  */
 package org.apache.fineract.portfolio.self.client.service;
 
+import lombok.RequiredArgsConstructor;
 import org.apache.fineract.infrastructure.security.service.PlatformSecurityContext;
 import org.apache.fineract.portfolio.client.exception.ClientNotFoundException;
 import org.apache.fineract.useradministration.domain.AppUser;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.stereotype.Service;
 
-@Service
+@RequiredArgsConstructor
 public class AppuserClientMapperReadServiceImpl implements AppuserClientMapperReadService {
 
     private final JdbcTemplate jdbcTemplate;
     private final PlatformSecurityContext context;
-
-    @Autowired
-    public AppuserClientMapperReadServiceImpl(final JdbcTemplate jdbcTemplate, final PlatformSecurityContext context) {
-        this.jdbcTemplate = jdbcTemplate;
-        this.context = context;
-    }
 
     @Override
     public Boolean isClientMappedToUser(Long clientId, Long appUserId) {
