0c1997aa9ab5806662965d2ecf5dc36e1fcbaca6
Mohamed Magdi-Abdelmonem <mohamed.magdi.abdelmonem@gmail.com>
2023-10-04 19:03:21 +0300

    FINERACT-1932: Add stater configuration class for spm module services

--- fineract-provider/src/main/java/org/apache/fineract/spm/service/LookupTableService.java
+++ fineract-provider/src/main/java/org/apache/fineract/spm/service/LookupTableService.java
@@ -19,25 +19,17 @@
 package org.apache.fineract.spm.service;
 
 import java.util.List;
+import lombok.RequiredArgsConstructor;
 import org.apache.fineract.infrastructure.security.service.PlatformSecurityContext;
 import org.apache.fineract.spm.domain.LookupTable;
 import org.apache.fineract.spm.domain.LookupTableRepository;
 import org.apache.fineract.spm.domain.Survey;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
-@Service
+@RequiredArgsConstructor
 public class LookupTableService {
 
     private final PlatformSecurityContext securityContext;
     private final LookupTableRepository lookupTableRepository;
-
-    @Autowired
-    public LookupTableService(final PlatformSecurityContext securityContext, final LookupTableRepository lookupTableRepository) {
-
-        this.securityContext = securityContext;
-        this.lookupTableRepository = lookupTableRepository;
-    }
 
     public List<LookupTable> findByKey(final String key) {
         this.securityContext.authenticatedUser();
