bf337c9b37e6ead3aaf85c622626e4c890fb014d
alex-mbolonzi <almbolonzi@gail.com>
2023-11-13 18:36:22 +0300

    hikari config updates

--- fineract-core/src/main/java/org/apache/fineract/infrastructure/core/service/migration/TenantDataSourceFactory.java
+++ fineract-core/src/main/java/org/apache/fineract/infrastructure/core/service/migration/TenantDataSourceFactory.java
@@ -67,7 +67,7 @@ public class TenantDataSourceFactory {
         String protocol = toProtocol(tenantDataSource);
 //        String tenantJdbcUrl = toJdbcUrl(protocol, tenantConnection.getSchemaServer(), tenantConnection.getSchemaServerPort(),
 //                tenantConnection.getSchemaName(), tenantConnection.getSchemaConnectionParameters());
-        String tenantJdbcUrl = toJdbcUrlGCP(protocol, tenantConnection.getSchemaName(), tenantConnection.getSchemaConnectionParameters());
+        String tenantJdbcUrl = toJdbcUrlGCP(protocol, tenantConnection.getSchemaName(), tenantDataSource.getDataSourceProperties().getProperty("cloudSqlInstance"));
         LOG.info("JDBC URL for tenant {} is {}", tenant.getTenantIdentifier(), tenantJdbcUrl);
         dataSource.setJdbcUrl(tenantJdbcUrl);
         return dataSource;
