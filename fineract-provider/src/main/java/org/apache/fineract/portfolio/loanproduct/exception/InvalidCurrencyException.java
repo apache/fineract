97dde50f1cb1bc1238aca1a71a854de92dd1ee50
Adam Saghy <adamsaghy@gmail.com>
2023-12-15 17:17:22 +0100

    Revert "FINERACT-1932: Fineract modularization - fineract-savings"
    This reverts commit b9314d0b1e8cc213433e8869ee0fd625ddef0250.

--- fineract-provider/src/main/java/org/apache/fineract/portfolio/loanproduct/exception/InvalidCurrencyException.java
+++ fineract-provider/src/main/java/org/apache/fineract/portfolio/loanproduct/exception/InvalidCurrencyException.java
@@ -16,7 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-package org.apache.fineract.organisation.monetary.exception;
+package org.apache.fineract.portfolio.loanproduct.exception;
 
 import org.apache.fineract.infrastructure.core.exception.AbstractPlatformDomainRuleException;
 
--- fineract-provider/src/main/java/org/apache/fineract/portfolio/savings/domain/SavingsAccountTransaction.java
+++ fineract-provider/src/main/java/org/apache/fineract/portfolio/savings/domain/SavingsAccountTransaction.java
@@ -46,6 +46,7 @@ import org.apache.fineract.infrastructure.core.service.DateUtils;
 import org.apache.fineract.organisation.monetary.domain.MonetaryCurrency;
 import org.apache.fineract.organisation.monetary.domain.Money;
 import org.apache.fineract.organisation.office.domain.Office;
+import org.apache.fineract.portfolio.note.domain.Note;
 import org.apache.fineract.portfolio.paymentdetail.domain.PaymentDetail;
 import org.apache.fineract.portfolio.savings.SavingsAccountTransactionType;
 import org.apache.fineract.portfolio.savings.data.SavingsAccountTransactionEnumData;
@@ -125,6 +126,10 @@ public final class SavingsAccountTransaction extends AbstractAuditableWithUTCDat
 
     @Column(name = "reason_for_block", nullable = true)
     private String reasonForBlock;
+
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "savings_account_transaction_id", referencedColumnName = "id")
+    private List<Note> notes = new ArrayList<>();
 
     @Column(name = "is_reversal", nullable = false)
     private boolean reversalTransaction;
@@ -421,6 +426,10 @@ public final class SavingsAccountTransaction extends AbstractAuditableWithUTCDat
 
     public List<SavingsAccountTransactionTaxDetails> getTaxDetails() {
         return this.taxDetails;
+    }
+
+    public List<Note> getNotes() {
+        return notes;
     }
 
     public Integer getTypeOf() {
