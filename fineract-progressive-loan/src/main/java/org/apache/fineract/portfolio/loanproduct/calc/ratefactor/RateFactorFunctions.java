adcfc3c8ba916ba55c9e83e8646c954ef03be442
Jose Alberto Hernandez <alberto@black-box.local>
2024-05-28 05:43:38 -0600

    FINERACT-1981: Function Value using simple interest for EMI calculation

--- fineract-progressive-loan/src/main/java/org/apache/fineract/portfolio/loanproduct/calc/ratefactor/RateFactorFunctions.java
+++ fineract-progressive-loan/src/main/java/org/apache/fineract/portfolio/loanproduct/calc/ratefactor/RateFactorFunctions.java
@@ -16,7 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-package org.apache.fineract.portfolio.loanproduct.ratefactor;
+package org.apache.fineract.portfolio.loanproduct.calc.ratefactor;
 
 import java.math.BigDecimal;
 import java.math.MathContext;
