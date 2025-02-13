a2742f9c50661c95aac8420904c3a0f5c61d7c8e
Víctor Romero <46640258+IOhacker@users.noreply.github.com>
2025-02-13 13:19:19 -0600

    Update ExternalEventConfigurationValidationService.java

--- fineract-core/src/main/java/org/apache/fineract/infrastructure/event/external/service/ExternalEventConfigurationValidationService.java
+++ fineract-core/src/main/java/org/apache/fineract/infrastructure/event/external/service/ExternalEventConfigurationValidationService.java
@@ -74,11 +74,11 @@ public class ExternalEventConfigurationValidationService implements Initializing
             log.debug("Missing from eventClasses: {}", CollectionUtils.subtract(eventClasses, eventConfigurations));
             log.debug("Missing from eventConfigurations: {}", CollectionUtils.subtract(eventConfigurations, eventClasses));
         }
-/*
+
         if (eventClasses.size() != eventConfigurations.size()) {
             throw new ExternalEventConfigurationNotFoundException();
         }
-*/
+
         for (String eventTypeClass : eventClasses) {
             if (!eventConfigurations.contains(eventTypeClass)) {
                 throw new ExternalEventConfigurationNotFoundException(eventTypeClass);
