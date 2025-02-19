e7c3906fc2364c9257c0c54ee725b175e78b3e73
a7med3del1973 <a7med3del1973@gmail.com>
2025-02-19 12:30:38 +0200

    FINERACT-2081: Return an empty collection instead of null.

--- custom/acme/note/service/src/main/java/com/acme/fineract/portfolio/note/service/AcmeNoteReadPlatformService.java
+++ custom/acme/note/service/src/main/java/com/acme/fineract/portfolio/note/service/AcmeNoteReadPlatformService.java
@@ -19,6 +19,7 @@
 package com.acme.fineract.portfolio.note.service;
 
 import java.util.Collection;
+import java.util.Collections;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.fineract.portfolio.note.data.NoteData;
 import org.apache.fineract.portfolio.note.service.NoteReadPlatformService;
@@ -43,6 +44,6 @@ public class AcmeNoteReadPlatformService implements NoteReadPlatformService, Ini
 
     @Override
     public Collection<NoteData> retrieveNotesByResource(Long resourceId, Integer noteTypeId) {
-        return null;
+        return Collections.emptyList();
     }
 }
