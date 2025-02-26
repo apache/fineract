7fc7a0f1998b0564af36e238bc3ef61d058a9b9a
a7med3del1973 <a7med3del1973@gmail.com>
2025-02-26 17:58:55 +0200

    FINERACT-2081: Added some tests for LogParameterEscapeUtil

--- fineract-provider/src/test/java/org/apache/fineract/infrastructure/security/utils/LogParameterEscapeUtilTest.java
+++ fineract-provider/src/test/java/org/apache/fineract/infrastructure/security/utils/LogParameterEscapeUtilTest.java
@@ -25,15 +25,27 @@ import io.cucumber.java8.En;
 public class LogParameterEscapeUtilTest implements En {
 
     private String logParameter;
-
     private String escapedLogParameter;
 
     public LogParameterEscapeUtilTest() {
         Given("A simple log message without any special character", () -> {
             logParameter = "This is a very simple String without any special character.";
         });
+
         Given("A log message with new line, carriage return and tab characters", () -> {
             logParameter = "This String contains new line\n, carriage return\r and tab\t characters.";
+        });
+
+        Given("A null log message", () -> {
+            logParameter = null;
+        });
+
+        Given("An empty log message", () -> {
+            logParameter = "";
+        });
+
+        Given("A log message with special characters {string}", (String input) -> {
+            logParameter = input;
         });
 
         When("Log parameter escape util escaping the special characters", () -> {
@@ -43,8 +55,13 @@ public class LogParameterEscapeUtilTest implements En {
         Then("The log message stays as it is", () -> {
             assertEquals(logParameter, escapedLogParameter);
         });
+
         Then("The escape util changes the special characters to `_`", () -> {
             assertEquals("This String contains new line_, carriage return_ and tab_ characters.", escapedLogParameter);
+        });
+
+        Then("The log message is escaped to {string}", (String expected) -> {
+            assertEquals(expected, escapedLogParameter);
         });
     }
 
