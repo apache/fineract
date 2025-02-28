c05c67e2ca128f1a7c9cc8923507f8f56e9cb90b
Victor Romero <victor.romero@fintecheando.mx>
2025-02-27 18:12:08 -0600

    TODO disable external events

--- fineract-provider/src/main/java/org/apache/fineract/infrastructure/configuration/domain/GlobalConfigurationRepositoryWrapper.java
+++ fineract-provider/src/main/java/org/apache/fineract/infrastructure/configuration/domain/GlobalConfigurationRepositoryWrapper.java
@@ -45,7 +45,7 @@ public class GlobalConfigurationRepositoryWrapper {
     public GlobalConfigurationProperty findOneByNameWithNotFoundDetection(final String propertyName) {
         final GlobalConfigurationProperty property = this.repository.findOneByName(propertyName);
         if (property == null) {
-            throw new GlobalConfigurationPropertyNotFoundException(propertyName);
+            //throw new GlobalConfigurationPropertyNotFoundException(propertyName);
         }
         return property;
     }
