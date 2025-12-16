36211d9c9b55f66e6fccb7984c6d19730bd66015
Dömötör D. Lugosi <lugalaci@gmail.com>
2025-12-17 00:39:47 +0100

    Update kubectl-shutdown.sh

--- kubernetes/kubectl-shutdown.sh
+++ kubernetes/kubectl-shutdown.sh
@@ -18,8 +18,20 @@
 # under the License.
 #
 
-kubectl delete secret fineract-tenants-db-secret
-kubectl delete -f fineractmysql-configmap.yml
-kubectl delete -f fineractmysql-deployment.yml
-kubectl delete -f fineract-server-deployment.yml
-kubectl delete -f fineract-mifoscommunity-deployment.yml
+echo "Shutting down Fineract Kubernetes deployment..."
+
+echo "Deleting mifos-community..."
+kubectl delete -f fineract-mifoscommunity-deployment.yml --ignore-not-found=true
+
+echo "Deleting fineract-server..."
+kubectl delete -f fineract-server-deployment.yml --ignore-not-found=true
+
+echo "Deleting fineractmysql..."
+kubectl delete -f fineractmysql-deployment.yml --ignore-not-found=true
+kubectl delete -f fineractmysql-configmap.yml --ignore-not-found=true
+
+echo "Deleting secrets..."
+kubectl delete secret fineract-tenants-db-secret --ignore-not-found=true
+
+echo
+echo "Fineract Kubernetes deployment has been shut down."
