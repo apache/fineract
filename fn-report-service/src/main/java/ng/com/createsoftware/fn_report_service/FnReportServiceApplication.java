1340715e03dbc243f1b279ca27fa36ff2145bc47
VAX <vakpo.okagbare@gmail.com>
2025-12-27 02:03:31 +0100

    Edit after specifying RabbitMQConfig in audit, asset and account officer

---
+++ fn-report-service/src/main/java/ng/com/createsoftware/fn_report_service/FnReportServiceApplication.java
@@ -0,0 +1,13 @@
+package ng.com.createsoftware.fn_report_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FnReportServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FnReportServiceApplication.class, args);
+	}
+
+}
---
+++ fn-report-service/src/test/java/ng/com/createsoftware/fn_report_service/FnReportServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ng.com.createsoftware.fn_report_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FnReportServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
