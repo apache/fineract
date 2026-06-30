5cc7f7da558fd926512643f8cbf8c3be0fe57a42
atharva-kamat <atharvakamat10@gmail.com>
2026-06-30 12:12:38 +0530

    Prevent Duplicate Command Handlers in DefaultCommandHandlerManager- Fail First at Startup

--- fineract-command/src/main/java/org/apache/fineract/command/implementation/DefaultCommandHandlerManager.java
+++ fineract-command/src/main/java/org/apache/fineract/command/implementation/DefaultCommandHandlerManager.java
@@ -18,14 +18,21 @@
  */
 package org.apache.fineract.command.implementation;
 
+import jakarta.annotation.PostConstruct;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.fineract.command.core.Command;
 import org.apache.fineract.command.core.CommandHandler;
 import org.apache.fineract.command.core.CommandHandlerManager;
+import org.apache.fineract.command.core.exception.CommandHandlerDuplicateException;
+import org.apache.fineract.command.core.exception.CommandHandlerInvalidRequestTypeException;
 import org.apache.fineract.command.core.exception.CommandHandlerNotFoundException;
+import org.springframework.aop.framework.AopProxyUtils;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.core.ResolvableType;
 import org.springframework.stereotype.Component;
 
 @Slf4j
@@ -37,6 +44,11 @@ public class DefaultCommandHandlerManager implements CommandHandlerManager {
 
     private final List<CommandHandler> handlers;
 
+    @PostConstruct
+    public void init() {
+        ensureCommandHandlerUniqueness(this.handlers);
+    }
+
     @Override
     public <REQ, RES> RES handle(Command<REQ> command) {
         CommandHandler<REQ, RES> handler = lookup(command);
@@ -45,12 +57,37 @@ public class DefaultCommandHandlerManager implements CommandHandlerManager {
     }
 
     private <REQ, RES> CommandHandler<REQ, RES> lookup(Command<REQ> command) {
-        // TODO: make sure there are no duplicate handlers
         if (command == null) {
             throw new CommandHandlerNotFoundException(command);
         }
 
         return handlers.stream().filter(handler -> handler.matches(command)).findFirst()
                 .orElseThrow(() -> new CommandHandlerNotFoundException(command));
+    }
+
+    private void ensureCommandHandlerUniqueness(List<CommandHandler> commandHandlers) {
+        final Set<Class<?>> registeredRequestTypes = new HashSet<>();
+
+        for (CommandHandler handler : commandHandlers) {
+            Class<?> userClass = AopProxyUtils.ultimateTargetClass(handler);
+
+            Class<?> requestType = ResolvableType.forClass(userClass).as(CommandHandler.class).getGeneric(0).resolve();
+
+            if (requestType == null) {
+                log.error("Warning: Could not resolve request payload type for handler: {}", userClass.getName());
+                throw new CommandHandlerInvalidRequestTypeException(userClass);
+            }
+
+            if (!registeredRequestTypes.add(requestType)) {
+
+                log.error("Critical Configuration ERROR: Duplicate CommandHandlers detected for request type [{}]. Conflict found for: {}",
+                        requestType.getName(), userClass.getSimpleName());
+
+                throw new CommandHandlerDuplicateException(requestType, userClass);
+            }
+        }
+
+        log.info("CommandHandler validation successful. Verified {} handler(s) uniquely map to their respective requests.",
+                commandHandlers.size());
     }
 }
