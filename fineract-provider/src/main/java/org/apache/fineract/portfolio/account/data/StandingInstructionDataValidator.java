bf6d6ad0f267118802d1e383768558295d31f125
Francisco Cuandon <francisco.cuandon@fintecheando.mx>
2026-08-06 23:06:28 -0600

    FINERACT-2048: Make amount and recurrence fields optional for Dues-based Standing Instructions

--- fineract-provider/src/main/java/org/apache/fineract/portfolio/account/data/StandingInstructionDataValidator.java
+++ fineract-provider/src/main/java/org/apache/fineract/portfolio/account/data/StandingInstructionDataValidator.java
@@ -81,6 +81,8 @@ public class StandingInstructionDataValidator {
     private final StandingInstructionHelper standingInstructionHelper;
     private final FromJsonHelper fromApiJsonHelper;
     private final AccountTransfersDetailDataValidator accountTransfersDetailDataValidator;
+    private final StandingInstructionValidatorHelper standingInstructionValidatorHelper;
+
     private static final Set<String> CREATE_REQUEST_DATA_PARAMETERS = new HashSet<>(
             Arrays.asList(localeParamName, dateFormatParamName, fromOfficeIdParamName, fromClientIdParamName, fromAccountTypeParamName,
                     fromAccountIdParamName, toOfficeIdParamName, toClientIdParamName, toAccountTypeParamName, toAccountIdParamName,
