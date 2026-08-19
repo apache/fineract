8f9377c1f0a675d8a3deef4933598d11465cfa77
Bhavya-Sonigra <sonigrabhavya28@gmail.com>
2026-08-19 22:38:52 +0530

    FINERACT-2774: Increase note length to TEXT
    Updates m_note table column to TEXT via Liquibase, removes 1000 character length constraint on JPA Note entity, and updates API NoteCreateRequest and NoteUpdateRequest max size validations to 65535.

--- fineract-core/src/main/java/org/apache/fineract/portfolio/note/data/NoteUpdateRequest.java
+++ fineract-core/src/main/java/org/apache/fineract/portfolio/note/data/NoteUpdateRequest.java
@@ -44,7 +44,7 @@ public class NoteUpdateRequest implements Serializable {
     private Long resourceId;
     @Hidden
     private NoteType type;
-    @Size(max = 1000, message = "{org.apache.fineract.portfolio.note.note.size}")
+    @Size(max = 65535, message = "{org.apache.fineract.portfolio.note.note.size}")
     @NotNull(message = "{org.apache.fineract.portfolio.note.note.not-null}")
     private String note;
 }
